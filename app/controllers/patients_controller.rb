1ba357e7e2fe7a1b952e58c757e680a1c4b77cea
Rick Zakharov <rick.zakharov@gmail.com>
2016-05-25 16:46:34 -0400

    Patient API - first draft

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,90 +1,27 @@
 class PatientsController < ApplicationController
-  # before_action :set_patient, only: [:show, :edit, :update, :destroy]
-
-
   # GET /patients/version
-def version
+  def version
     render json: 'Patient API version 0.1'
   end
 
   # GET /patients
   def index
-    render json: Patient.scan({}).collect { |data| data.to_h }
+    file = File.read('./data/patient_list.json')
+    data_hash = JSON.parse(file)
+    render json: data_hash
   end
 
-  # GET /patients
+  # GET /patients/test_file
   def test_file
-    render json: JSON.parse(File.new('./data/fake.json').to_s);
+    render json: 'Test File'
+    # render json: JSON.parse(File.new('./data/fake.json').to_s);
   end
 
-  # GET /patients
-  # GET /patients.json
-  # def index
-  #   @patients = Patient.all
-  # end
-  #
-  # # GET /patients/1
-  # # GET /patients/1.json
-  # def show
-  # end
-  #
-  # # GET /patients/new
-  # def new
-  #   @patient = Patient.new
-  # end
-  #
-  # # GET /patients/1/edit
-  # def edit
-  # end
-  #
-  # # POST /patients
-  # # POST /patients.json
-  # def create
-  #   @patient = Patient.new(patient_params)
-  #
-  #   respond_to do |format|
-  #     if @patient.save
-  #       format.html { redirect_to @patient, notice: 'Patient was successfully created.' }
-  #       format.json { render :show, status: :created, location: @patient }
-  #     else
-  #       format.html { render :new }
-  #       format.json { render json: @patient.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-  #
-  # # PATCH/PUT /patients/1
-  # # PATCH/PUT /patients/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @patient.update(patient_params)
-  #       format.html { redirect_to @patient, notice: 'Patient was successfully updated.' }
-  #       format.json { render :show, status: :ok, location: @patient }
-  #     else
-  #       format.html { render :edit }
-  #       format.json { render json: @patient.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-  #
-  # # DELETE /patients/1
-  # # DELETE /patients/1.json
-  # def destroy
-  #   @patient.destroy
-  #   respond_to do |format|
-  #     format.html { redirect_to patients_url, notice: 'Patient was successfully destroyed.' }
-  #     format.json { head :no_content }
-  #   end
-  # end
-  #
-  # private
-  #   # Use callbacks to share common setup or constraints between actions.
-  #   def set_patient
-  #     @patient = Patient.find(params[:id])
-  #   end
-  #
-  #   # Never trust parameters from the scary internet, only allow the white list through.
-  #   def patient_params
-  #     params.require(:patient).permit(:psn, :race, :gender, :step)
-  #   end
+  # GET /patients/1
+  # GET /patients/1.json
+  def show
+    file = File.read('./data/patient.json')
+    data_hash = JSON.parse(file)
+    render json: data_hash
+  end
 end
