b16f4965393c3113822f39541814cac63a5f6c8c
Rick Zakharov <rick.zakharov@gmail.com>
2016-05-25 17:06:17 -0400

    Added patient list, patient and patient timeline json mock data

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -11,10 +11,11 @@ class PatientsController < ApplicationController
     render json: data_hash
   end
 
-  # GET /patients/test_file
-  def test_file
-    render json: 'Test File'
-    # render json: JSON.parse(File.new('./data/fake.json').to_s);
+  # GET /patients/1/timeline
+  def timeline
+    file = File.read('./data/patient_timeline.json')
+    data_hash = JSON.parse(file)
+    render json: data_hash
   end
 
   # GET /patients/1
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'patients/version'
 
-  get 'patients/test_file' => 'patients#test_file'
   get 'patients', to: 'patients#index'
   get "patients/:id" => "patients#show"
+  get "patients/:id/timeline" => "patients#timeline"
 end
