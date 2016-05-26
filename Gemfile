8146bd9dace8632ac98837ab86ed47d694bbbd68
Rick Zakharov <rick.zakharov@gmail.com>
2016-05-26 11:59:58 -0400

    Added more unit tests

--- Gemfile
+++ Gemfile
@@ -53,4 +53,3 @@ group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
 end
-
--- spec/controllers/patient_controller_spec.rb
+++ spec/controllers/patient_controller_spec.rb
@@ -14,15 +14,33 @@ describe PatientsController do
   end
 
   it "GET /patients to return json list of patients" do
+    get :index, format: :json
 
+    expect(response).to have_http_status(200)
+
+    expect {
+      JSON.parse(response.body)
+    }.to_not raise_error
   end
 
   it "GET /patients/1 to return json patient" do
+    get :show, :id => "1", format: :json
 
+    expect(response).to have_http_status(200)
+
+    expect {
+      JSON.parse(response.body)
+    }.to_not raise_error
   end
 
   it "GET /patients/1/timeline to return json patient timeline" do
+    get :timeline, :id => "1", format: :json
 
+    expect(response).to have_http_status(200)
+
+    expect {
+      JSON.parse(response.body)
+    }.to_not raise_error
   end
 
 end
