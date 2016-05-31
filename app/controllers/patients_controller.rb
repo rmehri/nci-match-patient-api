1ec1885a4d9f8f6ff8960878d16e5296256b5e77
Rick Zakharov <rick.zakharov@gmail.com>
2016-05-31 17:29:15 -0400

    Fixed unit tests with mock data

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,62 +2,62 @@ class PatientsController < ApplicationController
   # before_action :authenticate
 
   # GET /patients
-  def index
-    begin
-      render json: Patient.scan({}).collect { |data| data.to_h }
-    rescue => error
-      standard_error_message(error)
-    end
-  end
   # def index
-  #   file = File.read('./data/patient_list.json')
-  #   data_hash = JSON.parse(file)
-  #   render json: data_hash
+  #   begin
+  #     render json: Patient.scan({}).collect { |data| data.to_h }
+  #   rescue => error
+  #     standard_error_message(error)
+  #   end
   # end
+  def index
+    file = File.read('./data/patient_list.json')
+    data_hash = JSON.parse(file)
+    render json: data_hash
+  end
 
   # GET /patients/1/timeline
-  def timeline
-    begin
-      if !params[:id].nil?
-        json_result = PatientEvent.scan(:scan_filter => {
-            "patient_id" => {
-                :comparison_operator => "EQ",
-                :attribute_value_list => [params[:id]]
-            }
-        }).collect { |data| data.to_h }
-      end
-      render json: json_result
-    rescue => error
-      standard_error_message(error)
-    end
-  end
   # def timeline
-  #   file = File.read('./data/patient_timeline.json')
-  #   data_hash = JSON.parse(file)
-  #   render json: data_hash
+  #   begin
+  #     if !params[:id].nil?
+  #       json_result = PatientEvent.scan(:scan_filter => {
+  #           "patient_id" => {
+  #               :comparison_operator => "EQ",
+  #               :attribute_value_list => [params[:id]]
+  #           }
+  #       }).collect { |data| data.to_h }
+  #     end
+  #     render json: json_result
+  #   rescue => error
+  #     standard_error_message(error)
+  #   end
   # end
+  def timeline
+    file = File.read('./data/patient_timeline.json')
+    data_hash = JSON.parse(file)
+    render json: data_hash
+  end
 
   # GET /patients/1
   # GET /patients/1.json
-  def show
-    begin
-      if !params[:id].nil?
-        json_result = Patient.scan(:scan_filter => {
-            "patient_id" => {
-                :comparison_operator => "EQ",
-                :attribute_value_list => [params[:id]]
-            }
-        }).collect { |data| data.to_h }
-      end
-      render json: json_result
-    rescue => error
-      standard_error_message(error)
-    end
-  end
   # def show
-  #   file = File.read('./data/patient.json')
-  #   data_hash = JSON.parse(file)
-  #   render json: data_hash
+  #   begin
+  #     if !params[:id].nil?
+  #       json_result = Patient.scan(:scan_filter => {
+  #           "patient_id" => {
+  #               :comparison_operator => "EQ",
+  #               :attribute_value_list => [params[:id]]
+  #           }
+  #       }).collect { |data| data.to_h }
+  #     end
+  #     render json: json_result
+  #   rescue => error
+  #     standard_error_message(error)
+  #   end
   # end
+  def show
+    file = File.read('./data/patient.json')
+    data_hash = JSON.parse(file)
+    render json: data_hash
+  end
 
 end
