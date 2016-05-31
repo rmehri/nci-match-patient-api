b8b8dedfba16952b46ecdd9bc4d6d379e620e76e
Rick Zakharov <rick.zakharov@gmail.com>
2016-05-31 14:23:44 -0400

    Added Patient model

---
+++ lib/item_operations.rb
@@ -0,0 +1,35 @@
+module Aws
+  module Record
+
+    module ItemOperations
+      module ItemOperationsClassMethods
+        alias_method :original_find, :find
+
+        def find(opts)
+          if (!self.table_exists?)
+            migration = Aws::Record::TableMigration.new(self)
+            migration.create!(provisioned_throughput: { read_capacity_units: 10, write_capacity_units: 10 })
+            migration.wait_until_available
+          end
+          original_find(opts)
+        end
+
+      end
+    end
+
+    module Query
+      module QueryClassMethods
+        alias_method :orginal_scan, :scan
+
+        def scan(opts = {})
+          if (!self.table_exists?)
+            migration = Aws::Record::TableMigration.new(self)
+            migration.create!(provisioned_throughput: { read_capacity_units: 10, write_capacity_units: 10 })
+            migration.wait_until_available
+          end
+          orginal_scan(opts)
+        end
+      end
+    end
+  end
+end
