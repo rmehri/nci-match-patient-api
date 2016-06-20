38b6c140b815c85a117269930ccecd328f7ff031
Rick Zakharov <rick.zakharov@gmail.com>
2016-06-20 17:31:31 -0400

    Synchronizing AWS Record table names between Patient API, State and Processor

--- config/initializers/model_config.rb
+++ config/initializers/model_config.rb
@@ -9,8 +9,12 @@ module ModelConfig
   end
 
   def self.configure_table(table)
-    table.set_table_name Config::Table.name table.new.class.name.underscore
-    # p table.table_name
+    name = table.new.class.name.underscore
+    name = name.to_s.split('/').last || ''
+
+    table.set_table_name "#{name}_#{Rails.env}"
+    # table.set_table_name Config::Table.name table.new.class.name.underscore
+    p table.table_name
   end
 
 end
