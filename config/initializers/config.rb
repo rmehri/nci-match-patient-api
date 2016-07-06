d19bbe54f2434e6751cfa5a8da6aab107e381aa5
Shan Yang <shan.yang@nih.gov>
2016-07-06 14:49:50 -0400

    Made table name pattern the same as in patient processor

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -8,8 +8,13 @@ module Config
 
   class Queue
     def self.name(name)
-      name = name.to_s.split('/').last || ''
-      ENV["queue_prefix"].to_s + name.to_s + ENV["queue_suffix"].to_s
+      # name = name.to_s.split('/').last || ''
+      # ENV["queue_prefix"].to_s + name.to_s + ENV["queue_suffix"].to_s
+
+      prefix = ENV["queue_prefix"]
+      prefix = if (prefix.nil? || prefix.length == 0) then '' else prefix + '_' end
+
+      name = "#{prefix}#{ENV["queue_name"]}_#{Rails.env}"
     end
   end
 end
