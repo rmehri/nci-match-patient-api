17521a2906116ec0dad58c2e459e6a98193c9604
Shan Yang <shan.yang@nih.gov>
2016-07-06 14:50:08 -0400

    Made table name pattern the same as in patient processor

--- config/initializers/model_config.rb
+++ config/initializers/model_config.rb
@@ -3,14 +3,12 @@ module ModelConfig
   def self.configure
     configure_table NciMatchPatientModels::Patient
     configure_table NciMatchPatientModels::PatientEvent
-    configure_table NciMatchPatientModels::Biopsy
     configure_table NciMatchPatientModels::Variant
     configure_table NciMatchPatientModels::VariantReport
     configure_table NciMatchPatientModels::Specimen
 
     ensure_table NciMatchPatientModels::Patient
     ensure_table NciMatchPatientModels::PatientEvent
-    ensure_table NciMatchPatientModels::Biopsy
     ensure_table NciMatchPatientModels::Variant
     ensure_table NciMatchPatientModels::VariantReport
     ensure_table NciMatchPatientModels::Specimen
@@ -20,9 +18,10 @@ module ModelConfig
     name = table.new.class.name.underscore
     name = name.to_s.split('/').last || ''
 
-    table.set_table_name "#{name}_#{Rails.env}"
-    # table.set_table_name Config::Table.name table.new.class.name.underscore
-    # p table.table_name
+    prefix = ENV["table_prefix"]
+    prefix = if (prefix.nil? || prefix.length == 0) then '' else prefix + '_' end
+
+    table.set_table_name "#{prefix}#{name}_#{Rails.env}"
   end
 
   def self.ensure_table(table)
