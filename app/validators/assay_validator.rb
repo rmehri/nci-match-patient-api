d91ac36ed8e2a03cc52e7be75a339b4ce9f6c17d
josephverbeck <joseph.verbeck@nih.gov>
2016-07-22 12:15:53 -0400

    Adding in more validation for api

--- app/validators/assay_validator.rb
+++ app/validators/assay_validator.rb
@@ -6,6 +6,8 @@ module MessageValidator
           "type" => "object",
           "required" => ["patient_id", "study_id", "surgical_event_id", "biomarker", "reported_date", "result"],
           "properties" => {
+              "study_id" => {"type" => "string", "minLength" => 1,
+                             "enum" => ["APEC1621"]},
               "patient_id" => {"type" => "string", "minLength" => 1},
               "surgical_event_id" => {"type" => "string", "minLength" => 1},
               "biomarker" => {"type" => "string", "minLength" => 1
--- app/validators/specimen_shipped_validator.rb
+++ app/validators/specimen_shipped_validator.rb
@@ -8,10 +8,14 @@ module MessageValidator
           "properties" => {
               "specimen_shipped" => {
                   "type" => "object",
-                  "required" => ["patient_id", "study_id", "type", "carrier", "tracking_id"],
+                  "required" => ["patient_id", "study_id", "surgical_event_id", "molecular_id" ,"type", "carrier", "tracking_id"],
                   "properties" => {
+                      "study_id" => {"type" => "string", "minLength" => 1,
+                                     "enum" => ["APEC1621"]},
                       "patient_id" => {"type" => "string", "minLength" => 1},
-                      "type" => {"type" => "string", "minLength" => 1, "enum" => ["BLOOD_DNA", "TISSUE_DNA_AND_CDNA", "SLIDE"]}
+                      "type" => {"type" => "string", "minLength" => 1, "enum" => ["BLOOD_DNA", "TISSUE_DNA_AND_CDNA", "SLIDE"]},
+                      "surgical_event_id" => {"type" => "string", "minLength" => 1},
+                      "molecular_id" => {"type" => "string", "minLength" => 1}
                   }
               }
           }
