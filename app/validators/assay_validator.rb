dae5d4fb1af34858a348a5205b49ba83b8c8b95c
Shan Yang <shan.yang@nih.gov>
2016-08-17 17:18:33 -0400

    Assay and pathology json validation is a la mode

--- app/validators/assay_validator.rb
+++ app/validators/assay_validator.rb
@@ -1,22 +1,22 @@
 module MessageValidator
-  class AssayValidator
+  class AssayValidator < AbstractValidator
+    include ActiveModel::Validations
+    include ActiveModel::Callbacks
 
-    def self.schema
-      @schema = {
-          "type" => "object",
-          "required" => ["patient_id", "study_id", "surgical_event_id", "biomarker", "reported_date", "result"],
-          "properties" => {
-              "study_id" => {"type" => "string", "minLength" => 1,
-                             "enum" => ["APEC1621"]},
-              "patient_id" => {"type" => "string", "minLength" => 1},
-              "surgical_event_id" => {"type" => "string", "minLength" => 1},
-              "biomarker" => {"type" => "string", "minLength" => 1
-                              # "enum" => ["PTen",
-                              #            "MSH",
-                              #            "Third"]
-              }
-          }
-      }
-    end
+    define_model_callbacks :from_json
+    after_from_json :include_correct_module
+
+    attr_accessor :study_id, :patient_id, :surgical_event_id, :biomarker, :reported_date, :ordered_date, :result
+
+    validates :study_id, presence: true, inclusion: { in: %w(APEC1621), message: "%{value} is not a valid study_id"} # reapeated so should be its own validation
+    validates :patient_id, presence: true
+    validates :surgical_event_id, presence: true
+    validates :biomarker, presence: true, inclusion: {in: %w(ICCPTENs ICCMLH1s), message: "%{value} is not a valid biomarker"}
+
+    validates :reported_date, presence: true
+    validates :ordered_date, presence: true
+    validates :result, presence: true, inclusion: {in: %w(POSITIVE NEGATIVE), message: "%{value} is not a valid assay result"}
+
   end
+
 end
--- app/validators/pathology_validator.rb
+++ app/validators/pathology_validator.rb
@@ -1,20 +1,18 @@
 module MessageValidator
-  class PathologyValidator
+  class PathologyValidator < AbstractValidator
+    include ActiveModel::Validations
+    include ActiveModel::Callbacks
 
-    def self.schema
-      @schema = {
-          "type" => "object",
-          "required" => ["patient_id", "surgical_event_id", "status", "reported_date"],
-          "properties" => {
-              "patient_id" => {"type" => "string", "minLength" => 1},
-              "surgical_event_id" => {"type" => "string", "minLength" => 1},
-              "status" => {"type" => "string", "minLength" => 1,
-                           "enum" => ["Y",
-                                      "N",
-                                      "U"]
-              }
-          }
-      }
-    end
+    define_model_callbacks :from_json
+    after_from_json :include_correct_module
+
+    attr_accessor :patient_id, :surgical_event_id, :status, :reported_date, :message
+
+    validates :patient_id, presence: true
+    validates :surgical_event_id, presence: true
+
+    validates :reported_date, presence: true
+    validates :status, presence: true, inclusion: {in: %w(Y N U), message: "%{value} is not a valid pathology review status"}
+
   end
 end
