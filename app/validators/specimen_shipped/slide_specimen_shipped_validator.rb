68d7104cd08ed3695e6a4cbf8414775da1a104d0
Shan Yang <shan.yang@nih.gov>
2016-08-26 11:05:51 -0400

    Slide can only shipped to MDA

--- app/validators/specimen_shipped/slide_specimen_shipped_validator.rb
+++ app/validators/specimen_shipped/slide_specimen_shipped_validator.rb
@@ -4,6 +4,7 @@ module SlideSpecimenShippedValidator
   included do
     validates :slide_barcode, presence: true
     validates :surgical_event_id, presence: true
+    validates :destination, presence: true, inclusion: {in: %w(MDA), message: "%{value} is not a valid shipping destination for SLIDE"}
   end
 
 end
