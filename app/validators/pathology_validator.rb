b2abc404e4ffa6bebee9a482f83b14f25292b301
josephverbeck <joseph.verbeck@nih.gov>
2016-08-29 14:20:55 -0400

    Fixing report date issue

--- app/validators/pathology_validator.rb
+++ app/validators/pathology_validator.rb
@@ -13,7 +13,7 @@ module MessageValidator
     validates :study_id, presence: true, inclusion: { in: %w(APEC1621), message: "%{value} is not a valid study_id"}
     validates :surgical_event_id, presence: true
 
-    validates :reported_date, presence: true
+    validates :reported_date, presence: true, date: {on_or_before: lambda {DateTime.current.utc}}
     validates :status, presence: true, inclusion: {in: %w(Y N U), message: "%{value} is not a valid pathology review status"}
     validates :case_number, presence: true
     validates :type, inclusion: {in: %w(PATHOLOGY_STATUS), message: "%{value} is not a valid pathology message type"}
