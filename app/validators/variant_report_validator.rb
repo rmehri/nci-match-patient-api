e85cf9ac0e366e2afaa32c66ec39bbfad0f254fa
Shan Yang <shan.yang@nih.gov>
2016-09-08 13:35:17 -0400

    Added patient id in validator

--- app/validators/variant_report_validator.rb
+++ app/validators/variant_report_validator.rb
@@ -7,6 +7,7 @@ module MessageValidator
     after_from_json :include_correct_module
 
     attr_accessor :site,
+                  :patient_id,
                   :molecular_id,
                   :analysis_id,
                   :tsv_file_name,
@@ -14,12 +15,14 @@ module MessageValidator
                   :dna_bam_file_name,
                   :cdna_bam_file_name,
                   :dna_bai_file_name,
-                  :cdna_bai_file_path_name
+                  :cdna_bai_file_name
 
 
     validates :site, presence: true
     validates :molecular_id, presence: true
     validates :analysis_id, presence: true
+
+    #TODO
 
     # validates :tsv_file_name, allow_nil: true, if (!:dna_bam_file_name.nil? || !:cdna_bam_file_name.nil?)
 
