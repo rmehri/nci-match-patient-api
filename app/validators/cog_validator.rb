7f9b3117b2aef62cf4ced32eae9ff9c2d0d33ffe
Shan Yang <shan.yang@nih.gov>
2016-09-15 15:44:39 -0400

    Added validation for REQUEST_ASSIGNMENT

--- app/validators/cog_validator.rb
+++ app/validators/cog_validator.rb
@@ -7,11 +7,14 @@ module MessageValidator
     after_from_json :include_correct_module
 
     attr_accessor :header, :study_id, :patient_id, :step_number, :registration_date, :status, :status_date, :internal_use_only,
-                  :treatment_arm_id, :treatment_arm_version, :stratum_id, :assignment_date, :message
+                  :treatment_arm_id, :treatment_arm_version, :stratum_id, :assignment_date, :message, :rebiopsy
 
     validates :header, presence: true
     validates :study_id, presence: true, inclusion: { in: %w(APEC1621), message: "%{value} is not a valid study_id"}
     validates :patient_id, presence: true
+    validates :status, presence: true, inclusion: { in: %w(REGISTRATION ON_TREATMENT_ARM REQUEST_ASSIGNMENT REQUEST_NO_ASSIGNMENT OFF_STUDY),
+                                                    message: "%{value} is not supported COG status value"}
+
 
     def include_correct_module
       case @status.to_sym
@@ -21,8 +24,10 @@ module MessageValidator
           class << self; include OnTreatmentArmValidator end
         when :OFF_STUDY
           class << self; include OffStudyValidator end
+        when :REQUEST_ASSIGNMENT
+          class << self; include RequestAssignmentValidator end
         else
-          raise "This is not a message from COG"
+          p "Cog message status is: #{@status}"
       end
 
     end
--- app/validators/request_assignment_validator.rb
+++ app/validators/request_assignment_validator.rb
@@ -1,11 +1,8 @@
-
-module OnTreatmentArmValidator
+module RequestAssignmentValidator
   extend ActiveSupport::Concern
 
   included do
 
-    validates :treatment_arm_id, presence: true
-    validates :stratum_id, presence: true
     validates :internal_use_only, presence: true
     validates :step_number, presence: true
     validates :status_date, presence: true
