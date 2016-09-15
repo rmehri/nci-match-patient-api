5c63847bfc7eac11bdde2d9cdca663f4f8989224
Shan Yang <shan.yang@nih.gov>
2016-09-15 11:27:20 -0400

    treatment_arm_version should not be in COG message

--- app/validators/on_treatment_arm_validator.rb
+++ app/validators/on_treatment_arm_validator.rb
@@ -5,7 +5,6 @@ module OnTreatmentArmValidator
   included do
     validates :status, presence: true, inclusion: { in: %w(ON_TREATMENT_ARM), message: "%{value} is not supported by a ON_TREATMENT_ARM message"}
     validates :treatment_arm_id, presence: true
-    validates :treatment_arm_version, presence: true
     validates :stratum_id, presence: true
     validates :internal_use_only, presence: true
     validates :step_number, presence: true
