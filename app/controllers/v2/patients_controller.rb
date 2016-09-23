6f827cfd55182f1056ff1600659e96350273432f
josephverbeck <joseph.verbeck@nih.gov>
2016-09-23 09:43:16 -0400

    Fixing query

--- app/controllers/v2/patients_controller.rb
+++ app/controllers/v2/patients_controller.rb
@@ -1,6 +1,8 @@
 module V2
   class PatientsController < V2::BaseController
 
+    #attributes_to_get: Array
+    #conditional_operator: string(AND/OR)
     private
     def patient_params
       params.require(:patient_id).permit(:registration_date)
@@ -8,16 +10,17 @@ module V2
 
     def query_params
       parameters = params.permit(:patient_id, :registration_date, :study_id, :gender, :ethnicity, :current_step_number, :current_status, :treating_site_id, :message)
-      {table_name: @resource_name, :scan_filter => build_scan_filter(parameters)}
+      build_query(parameters)
+    end
+
+    def build_query(params)
+      {table_name: @resource_name, :scan_filter => build_scan_filter(params)}
     end
 
     def build_scan_filter(params)
       query = {}
       params.each do |key , value|
         query.merge!(key => {:comparison_operator => "EQ", :attribute_value_list => [value]})
-      end
-      if(params.length >= 2)
-        query.merge!(:conditional_operator => "AND")
       end
       query
     end
