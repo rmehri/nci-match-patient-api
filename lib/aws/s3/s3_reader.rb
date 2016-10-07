16568df24153e8dffd70118dc8f2ce8d2a66fc36
Shan Yang <shan.yang@nih.gov>
2016-10-07 11:09:51 -0400

    Added stub for download()

--- lib/aws/s3/s3_reader.rb
+++ lib/aws/s3/s3_reader.rb
@@ -5,18 +5,23 @@ module Aws
 
       def self.read(bucket_name, path_to_file)
 
-        @s3_client ||= Aws::S3::Resource.new(endpoint: "https://s3.amazonaws.com",
-                                             region: Aws.config[:region],
-                                             access_key_id: Aws.config[:access_key_id],
-                                             secret_access_key: Aws.config[:secret_access_key]
-        )
-
-        resource = @s3_client.bucket(bucket_name).object(path_to_file)
+        resource = self.s3_client.bucket(bucket_name).object(path_to_file)
         raise "Specified resource path or name does not exist" if !resource.exists?
 
         resource.get({}).body.read
       end
 
+      def self.download(bucket_name, path_to_file)
+
+      end
+
+      def self.s3_client
+        @s3_client ||= Aws::S3::Resource.new(endpoint: "https://s3.amazonaws.com",
+                                             region: Aws.config[:region],
+                                             access_key_id: Aws.config[:access_key_id],
+                                             secret_access_key: Aws.config[:secret_access_key]
+        )
+      end
     end
   end
 end
