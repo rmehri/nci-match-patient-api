a7eb2a97cbccbe551ff54bb9dbf56d7d82d5ad0a
josephverbeck <joseph.verbeck@nih.gov>
2016-10-11 15:25:43 -0400

    Adding in unit tests to test meat of specimen events

--- spec/controllers/v1/specimen_events_controller_spec.rb
+++ spec/controllers/v1/specimen_events_controller_spec.rb
@@ -10,6 +10,30 @@ describe V1::SpecimenEventsController do
     expect(:get => "api/v1/patients/1/specimen_events").to route_to(:controller => "v1/specimen_events", :action => "index", :patient_id => "1")
   end
 
+  it 'GET #index should return something' do
+    get :index, :patient_id => "1234"
+    expect(response).to have_http_status(200)
+    expect(response.body).to eq("[]")
+  end
+
+  it 'GET #index with actual data TISSUE' do
+    allow(NciMatchPatientModels::Specimen).to receive(:scan).and_return([{:surgical_event_id => "1234", :type => "TISSUE", :specimen_shipments => []}])
+    allow(NciMatchPatientModels::Shipment).to receive(:scan).and_return([:molecular_id => "msn-1234"])
+    allow(NciMatchPatientModels::VariantReport).to receive(:scan).and_return([])
+    get :index, :patient_id => "1234"
+    expect(response).to have_http_status(200)
+    expect(response.body).not_to be_blank
+  end
+
+  it 'GET #index with actual data BLOOD' do
+    allow(NciMatchPatientModels::Specimen).to receive(:scan).and_return([{:surgical_event_id => "1234", :type => "BLOOD", :specimen_shipments => []}])
+    allow(NciMatchPatientModels::Shipment).to receive(:scan).and_return([:molecular_id => "msn-1234"])
+    allow(NciMatchPatientModels::VariantReport).to receive(:scan).and_return([])
+    get :index, :patient_id => "1234"
+    expect(response).to have_http_status(200)
+    expect(response.body).to include("specimen_shipments\":[]")
+  end
+
   it 'POST #create' do
     expect { post :create, :id => 1}.to raise_error(ActionController::UrlGenerationError)
   end
