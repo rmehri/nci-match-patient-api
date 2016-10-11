89cf283b399570be4844e02edfdb294cc6199807
josephverbeck <joseph.verbeck@nih.gov>
2016-10-10 23:57:54 -0400

    Fixing qc unit test

--- spec/controllers/v1/qc_variant_reports_controller_spec.rb
+++ spec/controllers/v1/qc_variant_reports_controller_spec.rb
@@ -9,6 +9,16 @@ describe V1::QcVariantReportsController do
                                                                                       :id => "analysis123")
   end
 
+  it 'GET #show 404 error' do
+    expect(get :show, :patient_id => "3366", :id => "1" ).to have_http_status(404)
+  end
+
+  it 'GET #show correctly' do
+    allow(NciMatchPatientModels::VariantReport).to receive(:scan).and_return([{:tsv_file_name => "random"}])
+    allow(Aws::S3::S3Reader).to receive(:read).and_return("BLAH BLAH")
+    expect( get :show, :patient_id => "3366", :id => "1").to have_http_status(200)
+  end
+
   it 'should not route' do
     expect { get :index, :patient_id => "3366", :id => "1"}.to raise_error(ActionController::UrlGenerationError)
   end
