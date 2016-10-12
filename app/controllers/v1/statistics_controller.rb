32149086836e92ff1f1143cb30b061dd46a83a8b
josephverbeck <joseph.verbeck@nih.gov>
2016-10-12 13:47:38 -0400

    Removing unnessary check

--- app/controllers/v1/statistics_controller.rb
+++ app/controllers/v1/statistics_controller.rb
@@ -9,13 +9,10 @@ module V1
 
         treatment_arm_accrual = build_treatment_arm_accrual(patientsOnTreatmentArm_dbm)
 
-        variant_report_dbm = NciMatchPatientModels::VariantReport.find_by({"status" => "CONFIRMED", "variant_report_type" => "TISSUE"}).collect {|x| x.patient_id}.uniq
-        AppLogger.log_debug(self.class.name, "Got #{variant_report_dbm.length} variant reports with status='CONFIRMED' and variant_report_type='TISSUE'")
-
         render json: {
             :number_of_patients => patient_dbm.length.to_s,
             :number_of_patients_on_treatment_arm => patientsOnTreatmentArm_dbm.length.to_s,
-            :number_of_patients_with_confirmed_variant_report => variant_report_dbm.length.to_s,
+            :number_of_patients_with_confirmed_variant_report => NciMatchPatientModels::VariantReport.find_by({"status" => "CONFIRMED", "variant_report_type" => "TISSUE"}).collect {|x| x.patient_id}.uniq.length.to_s,
             :treatment_arm_accrual => treatment_arm_accrual
         }
 
@@ -48,7 +45,7 @@ module V1
     private
     def build_treatment_arm_accrual(patients_on_treatment_arm = {})
       treatment_arm_accrual = {}
-      Hash(patients_on_treatment_arm).select{ |x| x.has_key?(:current_assignment) }.each do | patient |
+      patients_on_treatment_arm.select{ |x| x.has_key?(:current_assignment) }.each do | patient |
         Hash(patient.current_assignment[:selected_treatment_arm]).each do | selected_arm |
           taKey = selected[:treatment_arm_id] + ' (' + selected[:stratum_id] + ', ' + selected[:version] + ')'
           if (treatment_arm_accrual.has_key?(taKey))
