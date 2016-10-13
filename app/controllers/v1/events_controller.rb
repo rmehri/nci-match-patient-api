ca5a6f0e395e9c1b89bd5ba37999524259dbf56b
josephverbeck <joseph.verbeck@nih.gov>
2016-10-13 14:54:47 -0400

    Adding downcase so we will always get desc if described

--- app/controllers/v1/events_controller.rb
+++ app/controllers/v1/events_controller.rb
@@ -5,7 +5,7 @@ module V1
       plural_resource_name = "@#{resource_name.pluralize}"
       resources = resource_class.scan(query_params).collect { |data| data.to_h.compact }
       resources.sort_by!{ |record| record[:event_date]} unless !params.has_key?(:order)
-      resources.reverse! if (params.has_key?(:order) && params[:order] == 'desc')
+      resources.reverse! if (params.has_key?(:order) && params[:order].downcase == 'desc')
       resources = resources.take(params[:num].to_i) unless !params.has_key?(:num)
       instance_variable_set(plural_resource_name, resources)
       render json: instance_variable_get(plural_resource_name)
