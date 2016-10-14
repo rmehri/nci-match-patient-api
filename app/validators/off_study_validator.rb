c1c0a71d448f5c5def562f0afa0fb7e759013523
josephverbeck <joseph.verbeck@nih.gov>
2016-10-14 14:45:17 -0400

    Adding OFF_STUDY_BIO_EXPIRED to acceptable message status

--- app/validators/off_study_validator.rb
+++ app/validators/off_study_validator.rb
@@ -2,7 +2,7 @@ module OffStudyValidator
   extend ActiveSupport::Concern
 
   included do
-    validates :status, presence: true, inclusion: { in: %w(OFF_STUDY), message: "%{value} is not supported by a OFF_STUDY message"}
+    validates :status, presence: true, inclusion: { in: %w(OFF_STUDY OFF_STUDY_BIOPSY_EXPIRED), message: "%{value} is not supported by a OFF_STUDY message"}
     validates :status_date, presence: true, date: { on_or_before: lambda {DateTime.current.utc}, message: "status_date can not be later than current date"}
     validates :internal_use_only, presence: true
     validates :step_number, presence: true
