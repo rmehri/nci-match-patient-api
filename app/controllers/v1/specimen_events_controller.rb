73b03d739970b6dc6029baae1af54084e502aed7
josephverbeck <joseph.verbeck@nih.gov>
2016-10-19 17:17:55 -0400

    Cleaning up return values

--- app/controllers/v1/specimen_events_controller.rb
+++ app/controllers/v1/specimen_events_controller.rb
@@ -19,11 +19,14 @@ module V1
     def embed_resources(resource ={})
       resource[:specimen_shipments] = NciMatchPatientModels::Shipment.scan(build_query({:surgical_event_id => resource[:surgical_event_id]})).collect { |data| data.to_h.compact }
       resource[:specimen_shipments].collect do | shipment |
-        shipment[:analyses] = NciMatchPatientModels::Assignment.scan(build_query({:molecular_id => shipment[:molecular_id], :projection => [:analysis_id, :status, :status_date, :comment_user, :comment]})).collect{|record| record.to_h.compact }
+        shipment[:analyses] = NciMatchPatientModels::Assignment.scan(build_query({:molecular_id => shipment[:molecular_id],
+                                                                                  :projection => [:analysis_id, :status,
+                                                                                                  :status_date, :comment_user,
+                                                                                                  :comment]})).collect{|record| record[:assignment_report_status] = record.delete(:status) }
         shipment[:analyses] += NciMatchPatientModels::VariantReport.scan(build_query({:molecular_id => shipment[:molecular_id],
                                                                                      :projection => [:variant_report_received_date, :dna_bam_path_name, :dna_bai_path_name,
                                                                                                      :vcf_path_name, :rna_bam_path_name, :rna_bai_path_name, :tsv_path_name,
-                                                                                                     :qc_report_url, :vr_chart_data_url]})).collect{|record| record.to_h.compact }
+                                                                                                     :status ,:qc_report_url, :vr_chart_data_url]})).collect{|record| record[:variant_report_status] = record.delete(:status) }
       end
     end
 
