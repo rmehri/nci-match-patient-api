5f1abd2529920e872c7fa59e534a539ea5053bc3
josephverbeck <joseph.verbeck@nih.gov>
2016-10-20 12:35:30 -0400

    fixing statistic code so it finds accrual correctly

--- app/controllers/v1/statistics_controller.rb
+++ app/controllers/v1/statistics_controller.rb
@@ -1,33 +1,12 @@
 module V1
   class StatisticsController < ApplicationController
 
-    # def patient_statistics
-    #   begin
-    #     patient_dbm = NciMatchPatientModels::Patient.find_by().collect {|r| r.to_h.deep_symbolize_keys}
-    #     AppLogger.log_debug(self.class.name, "Got #{patient_dbm.length} patients")
-    #     patientsOnTreatmentArm_dbm = patient_dbm.select {|x| x[:current_status] == 'ON_TREATMENT_ARM'}
-    #
-    #     treatment_arm_accrual = build_treatment_arm_accrual(patientsOnTreatmentArm_dbm)
-    #
-    #     render json: {
-    #         :number_of_patients => patient_dbm.length.to_s,
-    #         :number_of_patients_on_treatment_arm => patientsOnTreatmentArm_dbm.length.to_s,
-    #         :number_of_patients_with_confirmed_variant_report => NciMatchPatientModels::VariantReport.find_by({"status" => "CONFIRMED", "variant_report_type" => "TISSUE"}).length.to_s,
-    #         :treatment_arm_accrual => treatment_arm_accrual
-    #     }
-    #
-    #   rescue => error
-    #     standard_error_message(error.message)
-    #   end
-    # end
-
     def patient_statistics
       begin
         patients_assignments = NciMatchPatientModels::Patient.scan({:scan_filter => {'current_status' =>
                                                                   {:comparison_operator => "EQ",
                                                                    :attribute_value_list => ['ON_TREATMENT_ARM']}},
-                                             :attributes_to_get => ['current_assignment']}).collect {|r| r.to_h.deep_symbolize_keys}
-
+                                             :attributes_to_get => ['current_assignment']}).collect {|r| r.to_h.deep_symbolize_keys!.compact}
         render json: {
             :number_of_patients => NciMatchPatientModels::Patient.scan({}).collect{}.length.to_s,
             :number_of_patients_on_treatment_arm => patients_assignments.length.to_s,
@@ -60,20 +39,25 @@ module V1
     end
 
     private
-    def build_treatment_arm_accrual(patients_assignments = {})
+
+    def build_treatment_arm_accrual(patients_assignments)
       begin
         treatment_arm_accrual = {}
         patients_assignments.each do |assignment|
-          if(assignment.has_key?(:selected_treatment_arm))
-            taKey = assignment[:selected_treatment_arm][:treatment_arm_id] + ' (' + assignment[:selected_treatment_arm][:stratum_id] + ', ' + assignment[:selected_treatment_arm][:version] + ')'
-            if(treatment_arm_accrual.has_key?(taKey))
-              treatment_arm_accrual[taKey][:patients] = treatment_arm_accrual[taKey][:patients] + 1
-            else
-              treatment_arm_accrual[taKey] = {
-                  :name => assignment[:selected_treatment_arm][:treatment_arm_id],
-                  :stratum_id => assignment[:selected_treatment_arm][:stratum_id],
-                  :patients => 1
-              }
+          if(assignment.has_key?(:current_assignment))
+            if(assignment[:current_assignment].has_key?(:selected_treatment_arm))
+              taKey = assignment[:current_assignment][:selected_treatment_arm][:treatment_arm_id] +
+                  ' (' + assignment[:current_assignment][:selected_treatment_arm][:stratum_id] +
+                  ', ' + assignment[:current_assignment][:selected_treatment_arm][:version] + ')'
+              if(treatment_arm_accrual.has_key?(taKey))
+                treatment_arm_accrual[taKey][:patients] = treatment_arm_accrual[taKey][:patients] + 1
+              else
+                treatment_arm_accrual[taKey] = {
+                    :name => assignment[:current_assignment][:selected_treatment_arm][:treatment_arm_id],
+                    :stratum_id => assignment[:current_assignment][:selected_treatment_arm][:stratum_id],
+                    :patients => 1
+                }
+              end
             end
           end
         end
