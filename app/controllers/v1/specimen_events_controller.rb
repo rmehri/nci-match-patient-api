5ef29840763d65ff581dd87baf13a4bb52708487
josephverbeck <joseph.verbeck@nih.gov>
2016-10-20 11:19:38 -0400

    Breaking the code up to build a model...will refactor at a later time

--- app/controllers/v1/specimen_events_controller.rb
+++ app/controllers/v1/specimen_events_controller.rb
@@ -19,20 +19,46 @@ module V1
     def embed_resources(resource ={})
       resource[:specimen_shipments] = NciMatchPatientModels::Shipment.scan(build_query({:surgical_event_id => resource[:surgical_event_id]})).collect { |data| data.to_h.compact }
       resource[:specimen_shipments].collect do | shipment |
-        shipment[:analyses] = NciMatchPatientModels::Assignment.scan(build_query({:molecular_id => shipment[:molecular_id], :projection => [:status, :status_date, :comment_user,:comment]})).collect{|record| record.to_h.compact}
-        shipment[:analyses].collect{|record| record[:assignment_report_status] = record.delete(:status)}
-        analyses = NciMatchPatientModels::VariantReport.scan(build_query({:molecular_id => shipment[:molecular_id],
+        shipment[:analyses]
+        assignment = NciMatchPatientModels::Assignment.scan(build_query({:molecular_id => shipment[:molecular_id], :projection => [:status, :status_date, :comment_user,:comment]})).collect{|record| record.to_h.compact}
+        variant_report = NciMatchPatientModels::VariantReport.scan(build_query({:molecular_id => shipment[:molecular_id],
                                                                                      :projection => [:analysis_id, :variant_report_received_date, :dna_bam_path_name, :dna_bai_path_name,
                                                                                                      :vcf_path_name, :rna_bam_path_name, :rna_bai_path_name, :tsv_path_name,
                                                                                                      :status ,:qc_report_url, :vr_chart_data_url]})).collect{|record| record.to_h.compact }
-        analyses.collect{ |record | record[:variant_report_status] = record.delete(:status)}
-        shipment[:analyses] += analyses
-
+        shipment[:analyses] = build_analyses_assignment_model(assignment.first).merge(build_variant_report_analyses_model(variant_report.first))
       end
     end
 
     def specimen_events_params
       build_query({:patient_id => params.require(:patient_id)})
+    end
+
+    def build_variant_report_analyses_model(variant_report)
+      variant_report ||= {}
+      {
+          :analysis_id => variant_report[:analysis_id],
+          :variant_report_status => variant_report[:status],
+          :variant_report_received_date => variant_report[:variant_report_received_date],
+          :dna_bam_path_name => variant_report[:dna_bam_path_name],
+          :dna_bai_path_name => variant_report[:dna_bai_path_name],
+          :vcf_path_name => variant_report[:vcf_path_name],
+          :rna_bam_path_name => variant_report[:rna_bam_path_name],
+          :rna_bai_path_name => variant_report[:rna_bai_path_name],
+          :tsv_path_name => variant_report[:tsv_path_name],
+          :qc_report_url => variant_report[:qc_report_url],
+          :vr_chart_data_url => variant_report[:vr_chart_data_url]
+      }
+    end
+
+    def build_analyses_assignment_model(assignment)
+      assignment ||= {}
+      {
+          :analysis_id => assignment[:analysis_id],
+          :assignment_report_status => assignment[:status],
+          :status_date => assignment[:status_date],
+          :comment_user => assignment[:comment_user],
+          :comment => assignment[:comment],
+      }
     end
 
   end
