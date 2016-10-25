c89816ae21cc04adcd7cbd9f3d366baf06a281ad
Shan Yang <shan.yang@nih.gov>
2016-10-24 22:02:03 -0400

    Fixed numbers on dashboard

--- lib/convert_assignment.rb
+++ lib/convert_assignment.rb
@@ -4,27 +4,30 @@ module Convert
       begin
         assignment = assignment_db.deep_symbolize_keys
 
+        puts "=========================== assignment: #{assignment}"
         assignment_results = assignment[:treatment_assignment_results]
 
-        assignment_logic = {}
-        assignment_results.each do |assignment_result|
-          ta = {:treatment_arm_id => assignment_result[:treatment_arm_id],
-                :stratum_id => assignment_result[:stratum_id],
-                :version => assignment_result[:version]}
-          assignment_result[:treatment_arm] = ta
-          assignment_result.delete(:treatment_arm_id)
-          assignment_result.delete(:stratum_id)
-          assignment_result.delete(:version)
-          assignment_logic[assignment_result[:assignment_status]] ||= []
-          assignment_logic[assignment_result[:assignment_status]].push(assignment_result)
+        if (!assignment_results.nil?)
+          assignment_logic = {}
+          assignment_results.each do |assignment_result|
+            ta = {:treatment_arm_id => assignment_result[:treatment_arm_id],
+                  :stratum_id => assignment_result[:stratum_id],
+                  :version => assignment_result[:version]}
+            assignment_result[:treatment_arm] = ta
+            assignment_result.delete(:treatment_arm_id)
+            assignment_result.delete(:stratum_id)
+            assignment_result.delete(:version)
+            assignment_logic[assignment_result[:assignment_status]] ||= []
+            assignment_logic[assignment_result[:assignment_status]].push(assignment_result)
         end
 
         assignment[:treatment_assignment_results] = assignment_logic
+        end
         assignment.delete(:patient) if !assignment[:patient].nil?
         assignment[:assays] = assays
         assignment
       rescue => error
-        Rails.logger.error(message.error)
+        Rails.logger.error(error.message)
       end
     end
   end
