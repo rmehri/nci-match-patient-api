7964a44c2c816b052c9ab0a7fb4faf33367ee50d
josephverbeck <joseph.verbeck@nih.gov>
2016-10-31 10:58:32 -0400

    Need status error to be passed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   def standard_error_message(error_message, error_code=500)
     logger.error error_message
-    redirect_to controller: 'errors', action: 'show', id: error_code, error_message: error_message
+    redirect_to controller: 'errors', action: 'show', id: error_code, error_message: error_message, status: error_code
   end
 
   def get_url_path_segments
