524574c018f929d12a4dfbb1f55aac693e02e6a7
josephverbeck <joseph.verbeck@nih.gov>
2016-11-04 11:02:55 -0400

    Fixing shipment query for one...still using scan for now

--- app/controllers/v1/shipments_controller.rb
+++ app/controllers/v1/shipments_controller.rb
@@ -2,11 +2,17 @@ module V1
   class ShipmentsController < BaseController
 
     private
+
+    def set_resource(resource = nil)
+      resource ||= resource_class.scan(resource_params).collect{|data| data.to_h.compact }
+      instance_variable_set("@#{resource_name}", resource)
+    end
+
     def shipments_params
-      build_query({:molecular_id => params.require(:id)})
-      # params.require(:id)
-      # # params[:molecular_id] = params.delete(:id)
-      # build_query(params.except(:action, :controller))
+      # build_query({:molecular_id => params.require(:id)})
+      params.require(:id)
+      params[:molecular_id] = params.delete(:id)
+      build_index_query(params.except(:action, :controller))
     end
 
   end
