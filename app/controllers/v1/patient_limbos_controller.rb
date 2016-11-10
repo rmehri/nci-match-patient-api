3dce2ea5eaa9246d86f559223e39e74e3288dd2b
josephverbeck <joseph.verbeck@nih.gov>
2016-11-10 12:04:15 -0500

    Adding in disease lookup

--- app/controllers/v1/patient_limbos_controller.rb
+++ app/controllers/v1/patient_limbos_controller.rb
@@ -8,9 +8,16 @@ module V1
 
     private
     def set_resource(resource = {})
-      resources = NciMatchPatientModels::Patient.scan({:attributes_to_get => ["active_tissue_specimen", "patient_id", "current_status"],
-                                                       :scan_filter => {"current_status" => {:comparison_operator => "IN", :attribute_value_list =>["AWAITING_PATIENT_DATA", "AWAITING_TREATMENT_ARM_STATUS"]},
-                                                       "active_tissue_specimen" => {:comparison_operator => "NOT_NULL"}}}).collect { |data| data.to_h.compact.deep_symbolize_keys! }
+      resources = NciMatchPatientModels::Patient.scan({:attributes_to_get => ["active_tissue_specimen", "patient_id", "current_status", "diseases"],
+                                                       :scan_filter => {"current_status" => {:comparison_operator => "IN", :attribute_value_list =>["AWAITING_PATIENT_DATA",
+                                                                                                                                                    "AWAITING_TREATMENT_ARM_STATUS",
+                                                                                                                                                    "PENDING_CONFIRMATION",
+                                                                                                                                                    "PENDING_APPROVAL",
+                                                                                                                                                    "REQUEST_ASSIGNMENT",
+                                                                                                                                                    "ON_TREATMENT_ARM"]},
+                                                       "active_tissue_specimen" => {:comparison_operator => "NOT_NULL"},
+                                                                        "diseases" => {:comparison_operator => "NULL"}},
+                                                       :conditional_operator => "AND"}).collect { |data| data.to_h.compact.deep_symbolize_keys! }
 
       resources.collect{ |resource| (Date.current - Date.parse(resource[:active_tissue_specimen][:slide_shipped_date])).to_i >= 2 }
       instance_variable_set("@#{resource_name}", resources)
