e43b13530f8bb3edc17b664820ab171dc4040efd
Shan Yang <shan.yang@nih.gov>
2016-12-01 15:09:53 -0500

    Updated messages

--- app/controllers/v1/patient_limbos_controller.rb
+++ app/controllers/v1/patient_limbos_controller.rb
@@ -8,7 +8,7 @@ module V1
 
     private
     def set_resource(resource = {})
-      # non_target_statuses = ["REGISTRATION", "ON_TREATMENT_ARM", "REQUEST_ASSIGNMENT", "REQUEST_NO_ASSIGNMENT", "OFF_STUDY", "OFF_STUDY_BIOPSY_EXPIRED"]
+      # non_target_statuses = ["REGISTRATION", "TISSUE_VARIANT_REPORT_CONFIRMED", "PENDING_CONFIRMATION", "ON_TREATMENT_ARM", "REQUEST_ASSIGNMENT", "REQUEST_NO_ASSIGNMENT", "OFF_STUDY", "OFF_STUDY_BIOPSY_EXPIRED"]
       # resources = NciMatchPatientModels::Patient.scan({:attributes_to_get => ["active_tissue_specimen", "patient_id", "current_status", "diseases", "message"],
       #                                                  :scan_filter => {
       #                                                  "active_tissue_specimen" => {:comparison_operator => "NOT_NULL"}}}).collect { |data| data.to_h.compact.deep_symbolize_keys! }
@@ -21,9 +21,7 @@ module V1
                          "TISSUE_SLIDE_SPECIMEN_SHIPPED",
                          "ASSAY_RESULTS_RECEIVED",
                          "TISSUE_VARIANT_REPORT_RECEIVED",
-                         "TISSUE_VARIANT_REPORT_CONFIRMED",
                          "TISSUE_VARIANT_REPORT_REJECTED",
-                         "PENDING_CONFIRMATION",
                          "PENDING_APPROVAL",
                          "AWAITING_PATIENT_DATA",
                          "AWAITING_TREATMENT_ARM_STATUS"]
@@ -44,37 +42,29 @@ module V1
         active_tissue_specimen = patient[:active_tissue_specimen]
 
         if (active_tissue_specimen[:active_molecular_id].nil?)
-          messages << "tissue shipment missing"
+          messages << "Tissue shipment missing"
         elsif active_tissue_specimen[:active_analysis_id].nil?
-          messages << "variant report missing"
+          messages << "Variant report missing"
         end
 
-        if (active_tissue_specimen[:slide_shipped_date].nil?)
-          messages << "slide shipment missing"
-        else
-          if (active_tissue_specimen[:ICCMLH1s].nil?)
-              messages << "MLH1 assay result missing"
-          end
+        if (active_tissue_specimen[:ICCMLH1s].nil?)
+            messages << "MLH1 assay result missing"
+        end
 
-          if (active_tissue_specimen[:ICCPTENs].nil?)
-            messages << "PTEN assay result missing"
-          end
+        if (active_tissue_specimen[:ICCPTENs].nil?)
+          messages << "PTEN assay result missing"
         end
 
         if (active_tissue_specimen[:variant_report_status].nil? || active_tissue_specimen[:variant_report_status] != 'CONFIRMED')
-          messages << "no confirmed variant report"
+          messages << "No confirmed variant report"
         end
 
         if (!patient[:message].nil?)
           messages << patient[:message]
         end
 
-        if (patient[:current_status] == 'PENDING_CONFIRMATION')
-          messages << "assignment report awaiting review"
-        end
-
         if (patient[:current_status] == 'PENDING_APPROVAL')
-          messages << "assignment report awaiting approval from COG"
+          messages << "Assignment report awaiting approval from COG"
         end
 
         patient[:message] = messages
