5311da0308da7ff1dfa5389800ecf787ec505463
Shan Yang <shan.yang@nih.gov>
2016-12-07 14:05:54 -0500

    Removed lowercase 'y' and 'n' in valid rebiopsy value list

--- app/validators/request_assignment_validator.rb
+++ app/validators/request_assignment_validator.rb
@@ -5,6 +5,6 @@ module RequestAssignmentValidator
     validates :internal_use_only, presence: true
     validates :step_number, presence: true
     validates :status_date, presence: true
-    validates :rebiopsy, allow_blank: true, inclusion: {in: %w(y Y n N), message: "%{value} is not supported"}
+    validates :rebiopsy, allow_blank: true, inclusion: {in: %w(Y N), message: "%{value} is not supported"}
   end
 end
