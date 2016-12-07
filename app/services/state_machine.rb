8e26dff4a5b79f97d5916a0408cff41918550ca0
josephverbeck <joseph.verbeck@nih.gov>
2016-12-07 15:51:31 -0500

    Adding token pass between api to state

--- app/services/state_machine.rb
+++ app/services/state_machine.rb
@@ -2,12 +2,12 @@ class StateMachine
   include HTTParty
   base_uri "#{Rails.configuration.environment.fetch('patient_state_api')}"
 
-  def self.validate(message)
+  def self.validate(message, token = "")
     begin
       result = post("/patientMessage",
                     {
                         :body => message.to_json,
-                        :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}
+                        :headers => { 'Authorization' => "Bearer #{token}", 'Content-Type' => 'application/json', 'Accept' => 'application/json'}
                     })
       result.body
     rescue Error => error
