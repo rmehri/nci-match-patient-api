8c400256a92b88be70c438a5fcef7c9ffccaf4d2
josephverbeck <joseph.verbeck@nih.gov>
2016-12-08 10:03:20 -0500

    removing old code

--- app/validators/assay_validator.rb
+++ app/validators/assay_validator.rb
@@ -14,7 +14,6 @@ module MessageValidator
     validates :study_id, presence: true, inclusion: { in: %w(APEC1621), message: "%{value} is not a valid study_id"} # reapeated so should be its own validation
     validates :patient_id, presence: true
     validates :surgical_event_id, presence: true
-    # validates :biomarker, presence: true, inclusion: {in: %w(ICCPTENs ICCBAF47s), message: "%{value} is not a valid biomarker"}
     validates :biomarker, presence: true, inclusion: {in: Rails.configuration.assay.collect{ |k, v| if(Date.parse(v["start_date"]) <= Date.current) && (Date.current <= Date.parse(v["end_date"])); k; end},
                                                       message: "%{value} is not a valid biomarker"}
 
@@ -24,12 +23,5 @@ module MessageValidator
     validates :type, inclusion: {in: %w(RESULT), message: "%{value} is not a valid assay message type"}
 
   end
-
-  # Rails.configuration.assay.each do | k, v |
-  #   if((Date.parse(v["start_date"]) <= Date.current) && (Date.current <= Date.parse(v["end_date"])))
-  #     raise "Unable to find assay #{k} for patient" if !@active_tissue_specimen.respond_to?(k.to_s)
-  #     raise "Assay #{k} cannot be blank" if @active_tissue_specimen.send(k).blank?
-  #   end
-  # end
 
 end
