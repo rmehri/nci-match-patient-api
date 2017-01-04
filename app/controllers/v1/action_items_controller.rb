2de94e6bacde8b4af855bfc466e2975ff3647c9a
asampatoor <adithya.sampatoor@nih.gov>
2017-01-03 21:02:04 -0500

    Show uuid in the action_items call & refactored code to look good

--- app/controllers/v1/action_items_controller.rb
+++ app/controllers/v1/action_items_controller.rb
@@ -6,27 +6,26 @@ module V1
       render json: instance_variable_get("@#{resource_name}")
     end
 
-
     private
+
     def set_resource(resources = {})
-      resources = NciMatchPatientModels::VariantReport.scan(resource_params).collect{ |record| build_model(record.to_h.compact)}
-      resources += NciMatchPatientModels::Assignment.scan(resource_params).collect{ | record | build_model(record.to_h.compact, "assignment_report") }
+      resources = NciMatchPatientModels::VariantReport.scan(resource_params).collect { |record| build_model(record.to_h.compact) }
+      resources += NciMatchPatientModels::Assignment.scan(resource_params).collect { |record| build_model(record.to_h.compact, 'assignment_report') }
       instance_variable_set("@#{resource_name}", resources)
     end
 
-
     def action_items_params
-      build_index_query({:patient_id => params.require(:patient_id), :status => 'PENDING', :variant_report_type => 'TISSUE'})
+      build_index_query(patient_id: params.require(:patient_id), status: 'PENDING', variant_report_type: 'TISSUE')
     end
 
     def build_model(record, type = 'variant_report')
       {
-          :action_type => "pending_#{(type == "variant_report") ? "#{record[:variant_report_type]}_#{type}" : type}".downcase,
-          :molecular_id => record[:molecular_id],
-          :analysis_id => record[:analysis_id],
-          :created_date => record[:status_date]
+        action_type: "pending_#{(type == "variant_report") ? "#{record[:variant_report_type]}_#{type}" : type}".downcase,
+        molecular_id: record[:molecular_id],
+        analysis_id: record[:analysis_id],
+        created_date: record[:status_date],
+        uuid: record[:uuid]
       }
     end
-
   end
 end
--- spec/controllers/v1/action_item_controller_spec.rb
+++ spec/controllers/v1/action_item_controller_spec.rb
@@ -1,55 +1,52 @@
 
-describe V1::ActionItemsController, :type => :controller do
+describe V1::ActionItemsController, type: :controller do
 
-  describe "should return valid data model" do
-    it "single VariantReport object return" do
-      allow(NciMatchPatientModels::VariantReport).to receive(:scan).and_return([{:molecular_id => "123", :analysis_id => "ABC", :variant_report_type=> "TISSUE", :status_date => Date.current}])
-      get :index, :patient_id => "123"
-      expect(JSON.parse(response.body)).to eq([{"action_type"=>"pending_tissue_variant_report", "molecular_id"=>"123", "analysis_id"=>"ABC", "created_date"=>Date.current.to_s}])
+  describe 'should return valid data model' do
+    it 'single VariantReport object return' do
+      allow(NciMatchPatientModels::VariantReport).to receive(:scan).and_return([{ molecular_id: '123', analysis_id: 'ABC', variant_report_type: 'TISSUE', status_date: Date.current, uuid: '1cbe6e30-ffa0-4842-8017-7deda9d19115' }])
+      get :index, patient_id: '123'
+      expect(JSON.parse(response.body)).to eq([{ 'action_type' => 'pending_tissue_variant_report', 'molecular_id' => '123', 'analysis_id' => 'ABC', 'created_date' => Date.current.to_s, 'uuid' =>  '1cbe6e30-ffa0-4842-8017-7deda9d19115' }])
     end
 
-    it "multiple VariantReport object return" do
-      allow(NciMatchPatientModels::VariantReport).to receive(:scan).and_return([{:molecular_id => "123", :analysis_id => "ABC", :variant_report_type=> "TISSUE", :status_date => Date.current},
-                                                                                {:molecular_id => "321", :analysis_id => "CBA", :variant_report_type=> "BLOOD", :status_date => Date.current}])
-      get :index, :patient_id => "123"
-      expect(JSON.parse(response.body)).to eq([{"action_type"=>"pending_tissue_variant_report", "molecular_id"=>"123", "analysis_id"=>"ABC", "created_date"=>Date.current.to_s}, {"action_type"=>"pending_blood_variant_report", "molecular_id"=>"321", "analysis_id"=>"CBA", "created_date"=>Date.current.to_s}])
+    it 'multiple VariantReport object return' do
+      allow(NciMatchPatientModels::VariantReport).to receive(:scan).and_return([{ molecular_id: '123', analysis_id: 'ABC', variant_report_type: 'TISSUE', status_date: Date.current, uuid: '1cbe6e30-ffa0-4842-8017-7deda9d19115' },
+                                                                                { molecular_id: '321', analysis_id: 'CBA', variant_report_type: 'BLOOD', status_date: Date.current, uuid: '03e6ebc1-0aff-2484-7108-8efab8e2625' }])
+      get :index, patient_id: '123'
+      expect(JSON.parse(response.body)).to eq([{ 'action_type' => 'pending_tissue_variant_report', 'molecular_id' => '123', 'analysis_id' => 'ABC', 'created_date' => Date.current.to_s, 'uuid' => '1cbe6e30-ffa0-4842-8017-7deda9d19115' }, {'action_type' => 'pending_blood_variant_report', 'molecular_id' => '321', 'analysis_id' => 'CBA', 'created_date' => Date.current.to_s, 'uuid' => '03e6ebc1-0aff-2484-7108-8efab8e2625' }])
     end
 
-    it "single Assignment object return" do
-      allow(NciMatchPatientModels::Assignment).to receive(:scan).and_return([{:molecular_id => "123", :analysis_id => "ABC", :status_date => Date.current}])
-      get :index, :patient_id => "123"
-      expect(JSON.parse(response.body)).to eq([{"action_type"=>"pending_assignment_report", "molecular_id"=>"123", "analysis_id"=>"ABC", "created_date"=>Date.current.to_s}])
+    it 'single Assignment object return' do
+      allow(NciMatchPatientModels::Assignment).to receive(:scan).and_return([{ molecular_id: '123', analysis_id: 'ABC', status_date: Date.current, uuid: '1cbe6e30-ffa0-4842-8017-7deda9d19115'}])
+      get :index, patient_id: '123'
+      expect(JSON.parse(response.body)).to eq([{ 'action_type' => 'pending_assignment_report', 'molecular_id' => '123', 'analysis_id' => 'ABC', 'created_date' => Date.current.to_s, 'uuid' => '1cbe6e30-ffa0-4842-8017-7deda9d19115' }])
     end
 
-    it "multiple Assignment object return" do
-      allow(NciMatchPatientModels::Assignment).to receive(:scan).and_return([{:molecular_id => "123", :analysis_id => "ABC", :status_date => Date.current},
-                                                                                {:molecular_id => "321", :analysis_id => "CBA", :status_date => Date.current}])
-      get :index, :patient_id => "123"
-      expect(JSON.parse(response.body)).to eq([{"action_type"=>"pending_assignment_report", "molecular_id"=>"123", "analysis_id"=>"ABC", "created_date"=>Date.current.to_s}, {"action_type"=>"pending_assignment_report", "molecular_id"=>"321", "analysis_id"=>"CBA", "created_date"=>Date.current.to_s}])
+    it 'multiple Assignment object return' do
+      allow(NciMatchPatientModels::Assignment).to receive(:scan).and_return([{ molecular_id: '123', analysis_id: 'ABC', status_date: Date.current, uuid: '1cbe6e30-ffa0-4842-8017-7deda9d19115'},
+                                                                             { molecular_id: '321', analysis_id: 'CBA', status_date: Date.current, uuid: '03e6ebc1-0aff-2484-7108-8efab8e2625'}])
+      get :index, patient_id: '123'
+      expect(JSON.parse(response.body)).to eq([{ 'action_type' => 'pending_assignment_report', 'molecular_id' => '123', 'analysis_id' => 'ABC', 'created_date' => Date.current.to_s, 'uuid' => '1cbe6e30-ffa0-4842-8017-7deda9d19115' }, { 'action_type' => 'pending_assignment_report', 'molecular_id' => '321', 'analysis_id' => 'CBA', 'created_date' => Date.current.to_s, 'uuid' => '03e6ebc1-0aff-2484-7108-8efab8e2625' }])
     end
 
-    it "mix and match multiple" do
-      allow(NciMatchPatientModels::Assignment).to receive(:scan).and_return([{:molecular_id => "321", :analysis_id => "CBA", :status_date => Date.current}])
-      allow(NciMatchPatientModels::VariantReport).to receive(:scan).and_return([{:molecular_id => "123", :analysis_id => "ABC", :variant_report_type=> "TISSUE", :status_date => Date.current}])
-      get :index, :patient_id => "123"
-      expect(JSON.parse(response.body)).to eq([{"action_type"=>"pending_tissue_variant_report", "molecular_id"=>"123", "analysis_id"=>"ABC", "created_date"=>Date.current.to_s}, {"action_type"=>"pending_assignment_report", "molecular_id"=>"321", "analysis_id"=>"CBA", "created_date"=>Date.current.to_s}])
+    it 'mix and match multiple' do
+      allow(NciMatchPatientModels::Assignment).to receive(:scan).and_return([{molecular_id: '321', analysis_id: 'CBA', status_date: Date.current, uuid: '03e6ebc1-0aff-2484-7108-8efab8e2625'}])
+      allow(NciMatchPatientModels::VariantReport).to receive(:scan).and_return([{molecular_id: '123', analysis_id: 'ABC', variant_report_type: 'TISSUE', status_date: Date.current, uuid: '03e6ebc1-0aff-2484-7108-8efab8e2625'}])
+      get :index, patient_id: '123'
+      expect(JSON.parse(response.body)).to eq([{ 'action_type' => 'pending_tissue_variant_report', 'molecular_id' => '123', 'analysis_id' => 'ABC', 'created_date' => Date.current.to_s, 'uuid' => '03e6ebc1-0aff-2484-7108-8efab8e2625'}, {'action_type' => 'pending_assignment_report', 'molecular_id' => '321', 'analysis_id' => 'CBA', 'created_date' => Date.current.to_s, 'uuid' => '03e6ebc1-0aff-2484-7108-8efab8e2625' }])
     end
   end
 
-
-  describe "should return empty when errors out" do
-    it "have valid http code and empty array return" do
+  describe 'should return empty when errors out' do
+    it 'have valid http code and empty array return' do
       allow(NciMatchPatientModels::Assignment).to receive(:scan).and_return([])
       allow(NciMatchPatientModels::VariantReport).to receive(:scan).and_return([])
-      get :index, :patient_id => "random"
+      get :index, patient_id: 'random'
       expect(response).to have_http_status(200)
-      expect(response.body).to eq("[]")
+      expect(response.body).to eq('[]')
     end
-
   end
 
-  it "should have a valid route" do
-    expect(:get => "api/v1/patients/1/action_items").to route_to(:controller => "v1/action_items", :action => "index", :patient_id => "1")
+  it 'should have a valid route' do
+    expect(get: 'api/v1/patients/1/action_items').to route_to(controller: 'v1/action_items', action: 'index', patient_id: '1')
   end
-
 end
