1c06cd3e66d21cbced2e1812e8b23940382530a7
asampatoor <adithya.sampatoor@nih.gov>
2017-01-04 11:30:31 -0500

    Fixing unit test cases

--- spec/controllers/v1/action_item_controller_spec.rb
+++ spec/controllers/v1/action_item_controller_spec.rb
@@ -5,34 +5,34 @@ describe V1::ActionItemsController, type: :controller do
     it 'single VariantReport object return' do
       allow(NciMatchPatientModels::VariantReport).to receive(:scan).and_return([{ molecular_id: '123', analysis_id: 'ABC', variant_report_type: 'TISSUE', status_date: Date.current, uuid: '1cbe6e30-ffa0-4842-8017-7deda9d19115' }])
       get :index, patient_id: '123'
-      expect(JSON.parse(response.body)).to eq([{ 'action_type' => 'pending_tissue_variant_report', 'molecular_id' => '123', 'analysis_id' => 'ABC', 'created_date' => Date.current.to_s, 'uuid' =>  '1cbe6e30-ffa0-4842-8017-7deda9d19115' }])
+      expect(JSON.parse(response.body)).to eq([{ 'action_type' => 'pending_tissue_variant_report', 'molecular_id' => '123', 'analysis_id' => 'ABC', 'created_date' => Date.current.to_s, 'assignment_uuid' =>  '1cbe6e30-ffa0-4842-8017-7deda9d19115' }])
     end
 
     it 'multiple VariantReport object return' do
       allow(NciMatchPatientModels::VariantReport).to receive(:scan).and_return([{ molecular_id: '123', analysis_id: 'ABC', variant_report_type: 'TISSUE', status_date: Date.current, uuid: '1cbe6e30-ffa0-4842-8017-7deda9d19115' },
                                                                                 { molecular_id: '321', analysis_id: 'CBA', variant_report_type: 'BLOOD', status_date: Date.current, uuid: '03e6ebc1-0aff-2484-7108-8efab8e2625' }])
       get :index, patient_id: '123'
-      expect(JSON.parse(response.body)).to eq([{ 'action_type' => 'pending_tissue_variant_report', 'molecular_id' => '123', 'analysis_id' => 'ABC', 'created_date' => Date.current.to_s, 'uuid' => '1cbe6e30-ffa0-4842-8017-7deda9d19115' }, {'action_type' => 'pending_blood_variant_report', 'molecular_id' => '321', 'analysis_id' => 'CBA', 'created_date' => Date.current.to_s, 'uuid' => '03e6ebc1-0aff-2484-7108-8efab8e2625' }])
+      expect(JSON.parse(response.body)).to eq([{ 'action_type' => 'pending_tissue_variant_report', 'molecular_id' => '123', 'analysis_id' => 'ABC', 'created_date' => Date.current.to_s, 'assignment_uuid' => '1cbe6e30-ffa0-4842-8017-7deda9d19115' }, {'action_type' => 'pending_blood_variant_report', 'molecular_id' => '321', 'analysis_id' => 'CBA', 'created_date' => Date.current.to_s, 'assignment_uuid' => '03e6ebc1-0aff-2484-7108-8efab8e2625' }])
     end
 
     it 'single Assignment object return' do
       allow(NciMatchPatientModels::Assignment).to receive(:scan).and_return([{ molecular_id: '123', analysis_id: 'ABC', status_date: Date.current, uuid: '1cbe6e30-ffa0-4842-8017-7deda9d19115'}])
       get :index, patient_id: '123'
-      expect(JSON.parse(response.body)).to eq([{ 'action_type' => 'pending_assignment_report', 'molecular_id' => '123', 'analysis_id' => 'ABC', 'created_date' => Date.current.to_s, 'uuid' => '1cbe6e30-ffa0-4842-8017-7deda9d19115' }])
+      expect(JSON.parse(response.body)).to eq([{ 'action_type' => 'pending_assignment_report', 'molecular_id' => '123', 'analysis_id' => 'ABC', 'created_date' => Date.current.to_s, 'assignment_uuid' => '1cbe6e30-ffa0-4842-8017-7deda9d19115' }])
     end
 
     it 'multiple Assignment object return' do
       allow(NciMatchPatientModels::Assignment).to receive(:scan).and_return([{ molecular_id: '123', analysis_id: 'ABC', status_date: Date.current, uuid: '1cbe6e30-ffa0-4842-8017-7deda9d19115'},
                                                                              { molecular_id: '321', analysis_id: 'CBA', status_date: Date.current, uuid: '03e6ebc1-0aff-2484-7108-8efab8e2625'}])
       get :index, patient_id: '123'
-      expect(JSON.parse(response.body)).to eq([{ 'action_type' => 'pending_assignment_report', 'molecular_id' => '123', 'analysis_id' => 'ABC', 'created_date' => Date.current.to_s, 'uuid' => '1cbe6e30-ffa0-4842-8017-7deda9d19115' }, { 'action_type' => 'pending_assignment_report', 'molecular_id' => '321', 'analysis_id' => 'CBA', 'created_date' => Date.current.to_s, 'uuid' => '03e6ebc1-0aff-2484-7108-8efab8e2625' }])
+      expect(JSON.parse(response.body)).to eq([{ 'action_type' => 'pending_assignment_report', 'molecular_id' => '123', 'analysis_id' => 'ABC', 'created_date' => Date.current.to_s, 'assignment_uuid' => '1cbe6e30-ffa0-4842-8017-7deda9d19115' }, { 'action_type' => 'pending_assignment_report', 'molecular_id' => '321', 'analysis_id' => 'CBA', 'created_date' => Date.current.to_s, 'assignment_uuid' => '03e6ebc1-0aff-2484-7108-8efab8e2625' }])
     end
 
     it 'mix and match multiple' do
       allow(NciMatchPatientModels::Assignment).to receive(:scan).and_return([{molecular_id: '321', analysis_id: 'CBA', status_date: Date.current, uuid: '03e6ebc1-0aff-2484-7108-8efab8e2625'}])
       allow(NciMatchPatientModels::VariantReport).to receive(:scan).and_return([{molecular_id: '123', analysis_id: 'ABC', variant_report_type: 'TISSUE', status_date: Date.current, uuid: '03e6ebc1-0aff-2484-7108-8efab8e2625'}])
       get :index, patient_id: '123'
-      expect(JSON.parse(response.body)).to eq([{ 'action_type' => 'pending_tissue_variant_report', 'molecular_id' => '123', 'analysis_id' => 'ABC', 'created_date' => Date.current.to_s, 'uuid' => '03e6ebc1-0aff-2484-7108-8efab8e2625'}, {'action_type' => 'pending_assignment_report', 'molecular_id' => '321', 'analysis_id' => 'CBA', 'created_date' => Date.current.to_s, 'uuid' => '03e6ebc1-0aff-2484-7108-8efab8e2625' }])
+      expect(JSON.parse(response.body)).to eq([{ 'action_type' => 'pending_tissue_variant_report', 'molecular_id' => '123', 'analysis_id' => 'ABC', 'created_date' => Date.current.to_s, 'assignment_uuid' => '03e6ebc1-0aff-2484-7108-8efab8e2625'}, {'action_type' => 'pending_assignment_report', 'molecular_id' => '321', 'analysis_id' => 'CBA', 'created_date' => Date.current.to_s, 'assignment_uuid' => '03e6ebc1-0aff-2484-7108-8efab8e2625' }])
     end
   end
 
