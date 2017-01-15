352215a74046f25922f25bf9aff6bde5d22f5293
Shan Yang <shan.yang@nih.gov>
2017-01-15 12:14:15 -0500

    Returns 404 if shipment not found

--- app/controllers/v1/shipment_status_controller.rb
+++ app/controllers/v1/shipment_status_controller.rb
@@ -9,10 +9,9 @@ module V1
     def set_resource(_resource = nil)
       puts "=============== params: #{params[:id]}"
       shipments = NciMatchPatientModels::Shipment.find_by({"molecular_id" => params[:id]}).collect { |data| data.to_h.compact.deep_symbolize_keys! }
-      return instance_variable_set("@#{resource_name}", {}) if shipments.length == 0
+      raise Errors::ResourceNotFound if shipments.length == 0
 
       patient_id = shipments.first[:patient_id]
-      puts "================ patient id: #{patient_id}"
 
       variant_reports = NciMatchPatientModels::VariantReport.query_by_patient_id(patient_id, false).collect { |data| data.to_h.compact.deep_symbolize_keys! }
       instance_variable_set("@#{resource_name}", construct_shipment_status(variant_reports, patient_id, params[:id]))
@@ -27,6 +26,9 @@ module V1
         shipment_status[:eligible_for_new_variant_report] = false if report[:status] == 'CONFIRMED'
       end
       shipment_status[:analysis_ids] = analysis_ids
+
+      puts "============= shipment status: #{shipment_status}"
+
       shipment_status
     end
   end
