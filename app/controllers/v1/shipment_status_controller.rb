6c94f7d1ab94ef04f7dbd7f514d1e72d6a63332a
Shan Yang <shan.yang@nih.gov>
2017-02-03 11:17:06 -0500

    Added site to shipment_status

--- app/controllers/v1/shipment_status_controller.rb
+++ app/controllers/v1/shipment_status_controller.rb
@@ -11,24 +11,24 @@ module V1
       shipments = NciMatchPatientModels::Shipment.find_by({"molecular_id" => params[:id]}).collect { |data| data.to_h.compact.deep_symbolize_keys! }
       raise Errors::ResourceNotFound if shipments.length == 0
 
-      patient_id = shipments.first[:patient_id]
-
-      variant_reports = NciMatchPatientModels::VariantReport.query_by_patient_id(patient_id, false).collect { |data| data.to_h.compact.deep_symbolize_keys! }
-      instance_variable_set("@#{resource_name}", construct_shipment_status(variant_reports, patient_id, params[:id]))
+      variant_reports = NciMatchPatientModels::VariantReport.query_by_patient_id(shipments.first[:patient_id], false).collect { |data| data.to_h.compact.deep_symbolize_keys! }
+      instance_variable_set("@#{resource_name}", construct_shipment_status(variant_reports, shipments.first, params[:id]))
     end
 
-    def construct_shipment_status(variant_reports, patient_id, molecular_id)
+    def construct_shipment_status(variant_reports, shipment, molecular_id)
       reports = variant_reports.select { | variant_report | variant_report[:molecular_id] == molecular_id}
       analysis_ids = []
-      shipment_status = {:patient_id => patient_id, :molecular_id => molecular_id, :eligible_for_new_variant_report => true}
+      shipment_status = {:patient_id => shipment[:patient_id], :molecular_id => molecular_id, :eligible_for_new_variant_report => true}
       reports.each do | report |
         analysis_ids << report[:analysis_id]
-        shipment_status[:eligible_for_new_variant_report] = false if report[:status] == 'CONFIRMED'
+        if report[:status] == 'CONFIRMED'
+          shipment_status[:eligible_for_new_variant_report] = false
+          shipment_status[:message] = "Molecular_id [#{molecular_id}] already has a confirmed variant report"
+        end
       end
+
       shipment_status[:analysis_ids] = analysis_ids
-
-      puts "============= shipment status: #{shipment_status}"
-
+      shipment_status[:site] = shipment[:destination]
       shipment_status
     end
   end
