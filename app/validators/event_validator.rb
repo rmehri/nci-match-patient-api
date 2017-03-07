6ac0a0efcd61ed16b98daeac0f7afa1e86741ff8
josephverbeck <joseph.verbeck@nih.gov>
2017-03-07 11:18:16 -0500

    Change to validator for event message

--- app/validators/event_validator.rb
+++ app/validators/event_validator.rb
@@ -7,12 +7,12 @@ module MessageValidator
     define_model_callbacks :from_json
     after_from_json :include_correct_module
 
-    attr_accessor :patient_id, :molecular_id, :analysis_id, :surgical_event_id, :dna_file_name, :rna_file_name, :vcf_file_name
+    attr_accessor :patient_id, :molecular_id, :analysis_id, :surgical_event_id, :dna_bam_name, :cdna_bam_name, :zip_name
 
     validates :molecular_id, presence: true
     validates :analysis_id, presence: true
-    validates :dna_file_name, file: [".bam"]
-    validates :rna_file_name, file: [".bam"]
-    validates :vcf_file_name, file: [".vcf", ".zip"]
+    validates :dna_bam_name, file: [".bam"]
+    validates :cdna_bam_name, file: [".bam"]
+    validates :zip_name, file: [".vcf", ".zip"]
   end
 end
--- spec/validators/event_validator_spec.rb
+++ spec/validators/event_validator_spec.rb
@@ -7,13 +7,13 @@ RSpec.describe MessageValidator::EventValidator do
   let (:valid_message) { {:patient_id => "123", :molecular_id => "123-mole",
                           :surgical_event_id => "123-surg", :analysis_id => "123-ana"}  }
   let (:valid_file_message) { {:patient_id => "123", :molecular_id => "123-mole",
-                          :surgical_event_id => "123-surg", :analysis_id => "123-ana", :rna_file_name => "test.bam",
-                               :dna_file_name => "test.bam", :vcf_file_name => "test.zip"} }
+                          :surgical_event_id => "123-surg", :analysis_id => "123-ana", :cdna_bam_name => "test.bam",
+                               :dna_bam_name => "test.bam", :zip_name => "test.zip"} }
 
   let (:invalid_message) { {:patient_id => "123"} }
   let (:invalid_file_message) { {:patient_id => "123", :molecular_id => "123-mole",
-                                 :surgical_event_id => "123-surg", :analysis_id => "123-ana", :rna_file_name => "test.zip",
-                                 :dna_file_name => "test.test", :vcf_file_name => "test.bam"} }
+                                 :surgical_event_id => "123-surg", :analysis_id => "123-ana", :cdna_bam_name => "test.zip",
+                                 :dna_bam_name => "test.test", :zip_name => "test.bam"} }
 
   context "initialize correctly" do
     it { expect(validator).to be_truthy }
