e877468e8d3749a1561527c72daaf009282cfddf
josephverbeck <joseph.verbeck@nih.gov>
2017-03-07 10:02:11 -0500

    Removing unused code

--- app/models/patient_ui_model.rb
+++ app/models/patient_ui_model.rb
@@ -1,8 +1,8 @@
-class PatientUiModel
-
-  attr_accessor :patient_id,:registration_date, :study_id, :gender, :ethnicity ,
-                :races, :current_step_number, :current_assignment, :current_status, :disease, :prior_drugs ,
-                :documents, :message, :timeline, :specimens, :biopsy_variant_report_map,
-                :variant_report_selectors, :variant_reports, :assignment_report
-
-end
+# class PatientUiModel
+#
+#   attr_accessor :patient_id,:registration_date, :study_id, :gender, :ethnicity ,
+#                 :races, :current_step_number, :current_assignment, :current_status, :disease, :prior_drugs ,
+#                 :documents, :message, :timeline, :specimens, :biopsy_variant_report_map,
+#                 :variant_report_selectors, :variant_reports, :assignment_report
+#
+# end
