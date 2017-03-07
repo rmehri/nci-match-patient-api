68518eb1e3e32a053262b699c953e684e2935c57
josephverbeck <joseph.verbeck@nih.gov>
2017-03-07 09:58:58 -0500

    Adding in start of test of patientUImodel

--- app/models/patient_ui_model.rb
+++ app/models/patient_ui_model.rb
@@ -1,29 +1,8 @@
 class PatientUiModel
 
-  attr_accessor :patient_id
-  attr_accessor :registration_date
-  attr_accessor :study_id
-  attr_accessor :gender
-  attr_accessor :ethnicity
-  attr_accessor :races
-  attr_accessor :current_step_number
-  attr_accessor :current_assignment
-  attr_accessor :current_status
-
-  attr_accessor :disease
-  attr_accessor :prior_drugs
-  attr_accessor :documents
-  attr_accessor :message
-
-  attr_accessor :timeline
-
-  attr_accessor :specimens
-
-  attr_accessor :biopsy_variant_report_map
-
-  attr_accessor :variant_report_selectors
-  attr_accessor :variant_reports
-
-  attr_accessor :assignment_report
+  attr_accessor :patient_id,:registration_date, :study_id, :gender, :ethnicity ,
+                :races, :current_step_number, :current_assignment, :current_status, :disease, :prior_drugs ,
+                :documents, :message, :timeline, :specimens, :biopsy_variant_report_map,
+                :variant_report_selectors, :variant_reports, :assignment_report
 
 end
