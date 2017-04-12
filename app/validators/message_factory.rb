472e812d354aebd343bf61415e88813a6762ee95
josephverbeck <joseph.verbeck@nih.gov>
2017-04-12 13:29:27 -0400

    Removing unused code

--- app/validators/message_factory.rb
+++ app/validators/message_factory.rb
@@ -1,19 +1,5 @@
 module MessageFactory
 
-  autoload :SpecimenReceivedValidator,     'specimen_received_validator'
-  autoload :SpecimenShippedValidator,      'specimen_shipped_validator'
-  autoload :CogValidator,                  'cog_validator'
-  autoload :OffStudyValidator,             'off_study_validator'
-  autoload :AssayValidator,                'assay_validator'
-  autoload :PathologyValidator,            'pathology_validator'
-  autoload :VariantReportValidator,        'variant_report_validator'
-  autoload :VariantReportStatusValidator,  'variant_report_status_validator'
-  autoload :TreatmentArmsValidator,        'treatment_arms_validator'
-  autoload :AssignmentStatusValidator,     'assignment_status_validator'
-  autoload :EventValidator,                'event_validator'
-  # autoload :RegistrationValidator,         'registration_validator'
-  autoload :AbstractValidator,             'abstract_validator'
-
   def self.get_message_type(message)
     case message.to_s
       when MessageValidator::SpecimenReceivedValidator.message_format
@@ -36,13 +22,6 @@ module MessageFactory
         type = MessageValidator::CogValidator
     end
     return type.new.from_json(message.to_json)
-  end
-
-  def self.validate_json_message(type)
-      unless type.valid?
-        return type.errors.messages
-      end
-      nil
   end
 
 end
