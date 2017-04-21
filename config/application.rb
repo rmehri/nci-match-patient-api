f654abd257c0150ed858b2bbdb45546beb060c70
Shan Yang <shan.yang@nih.gov>
2017-04-21 14:32:35 -0400

    Changed module name to NciPedMatchPatientApi

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require 'httparty'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module NciMatchPatientApi
+module NciPedMatchPatientApi
   class Application < Rails::Application
     config.autoload_paths += Dir[Rails.root.join('app', 'models', '*')]
     config.autoload_paths += Dir[Rails.root.join('app', 'models', '{*/}')]
