eface4aeb33407b41807c11c60b71d309ad09948
josephverbeck <joseph.verbeck@nih.gov>
2017-04-27 10:19:21 -0400

    Removing a test

--- spec/jobs/job_builder_spec.rb
+++ spec/jobs/job_builder_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe JobBuilder.new("Random") do
   it{is_expected.to be_truthy}
   it{expect(JobBuilder.new("Random").job).to be_truthy}
   it{expect(JobBuilder.new("Random").job).to be_kind_of(Object)}
+  # it{expect(JobBuilder.new("Random").job.respond_to?(:perform_later)).to be_truthy}
 
 
 end
