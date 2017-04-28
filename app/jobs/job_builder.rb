3a40e1111d041c70882e8e2be58844cc6be0eb91
josephverbeck <joseph.verbeck@nih.gov>
2017-04-28 12:05:53 -0400

    Adding in new code that builds Namespace on fly along with the job class to be enqueued

--- app/jobs/job_builder.rb
+++ app/jobs/job_builder.rb
@@ -13,7 +13,35 @@ class JobBuilder
     rescue NameError => error
       Rails.logger.info("Class #{name.classify} does NOT exist yet and will be created")
       Object.const_set(name.classify, Class.new(ActiveJob::Base) {queue_as "#{Rails.configuration.environment.fetch('queue_name')}"})
+      # name = name.split("::")
+      # if(name.length > 1)
+      #   create_class(name.pop, build_module(name).to_s)
+      # else
+      #   create_class(name.pop)
+      # end
     end
   end
+
+  # Specimen = Module.new
+  # Specimen::ReceivedJob = Class.new(ActiveJob::Base) {queue_as "#{Rails.configuration.environment.fetch('queue_name')}"} }
+  # Object.const_set("Specimen", Module.new)
+  # Object.const_get("Specimen").const_set("ReceivedJob", Class.new(ActiveJob::Base) {queue_as "#{Rails.configuration.environment.fetch('queue_name')}"} })
+
+  def create_class(class_name, name_space=nil)
+    unless name_space.blank?
+      Object.const_get(name_space).const_set(class_name, Class.new(ActiveJob::Base) {queue_as "#{Rails.configuration.environment.fetch('queue_name')}"})
+    end
+    Object.const_set(class_name, Class.new(ActiveJob::Base) {queue_as "#{Rails.configuration.environment.fetch('queue_name')}"})
+  end
+
+  #Builds out Namespace for class
+  #Example: given ["Cat", "Dog"] it will create a Cat::Dog module
+  def build_module(name)
+    return Object unless !name.blank?
+    current_name = name.pop
+    object = build_module(name)
+    object.const_set(current_name.classify, Module.new)
+  end
+
 
 end
