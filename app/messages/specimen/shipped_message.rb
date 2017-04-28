284270c2056afce223d4f18cccd10106745113eb
josephverbeck <joseph.verbeck@nih.gov>
2017-04-28 12:25:22 -0400

    Cleaing lambda to be ->

--- app/messages/specimen/shipped_message.rb
+++ app/messages/specimen/shipped_message.rb
@@ -8,7 +8,7 @@ module Specimen
                   :study_id, :surgical_event_id, :internal_use_only, :slide_barcode, :destination,
                   :molecular_id, :carrier, :tracking_id, :shipped_dttm
 
-    validates :shipped_dttm, presence: true, date: {on_or_before: lambda {DateTime.current.utc}}
+    validates :shipped_dttm, presence: true, date: {on_or_before: -> {DateTime.current.utc}}
 
     # Override
     def include_correct_module
