36ebf1e664a72a3569fea83ea632b93c78877eac
josephverbeck <joseph.verbeck@nih.gov>
2017-05-01 11:04:52 -0400

    Removing unused code

--- app/validators/message_validator.rb
+++ app/validators/message_validator.rb
@@ -14,46 +14,4 @@ module MessageValidator
   # autoload :RegistrationValidator,         'registration_validator'
   autoload :AbstractValidator,             'abstract_validator'
 
-  def self.get_message_type(message)
-    if (!message[:specimen_received].nil?)
-      type = "SpecimenReceived"
-    elsif (!message[:specimen_shipped].nil?)
-      type = "SpecimenShipped"
-    elsif (!message[:biomarker].nil?)
-      type = "Assay"
-    elsif (!message[:type].nil? && message[:type] == 'PATHOLOGY_STATUS')
-      type = "Pathology"
-    elsif (!message[:status_type].nil? && message[:status_type] == 'ASSIGNMENT')
-      type = "AssignmentStatus"
-    elsif (!message[:status].nil? && (message[:status] == 'CONFIRMED' || message[:status] == 'REJECTED'))
-      type = "VariantReportStatus"
-    elsif (!message[:analysis_id].nil?)
-      type = "VariantReport"
-    elsif (!message[:treatment_arms].nil?)
-      type = "TreatmentArm"
-    else
-      type = "Cog"
-    end
-
-    # Cog
-    # MDA
-    # NCH
-    # MATCH
-
-    type
-  end
-
-  def self.validate_json_message(type, message_json)
-    klazz = ("MessageValidator::" + type + "Validator").constantize
-    begin
-      message_validation = klazz.new.from_json(message_json.to_json)
-      unless message_validation.valid?
-        return message_validation.errors.messages
-      end
-      nil
-    rescue => error
-      error.message
-    end
-  end
-
 end
