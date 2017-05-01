7dbf0414d66b894d4d4f84f38abcfbfa8b701ceb
josephverbeck <joseph.verbeck@nih.gov>
2017-05-01 11:43:46 -0400

    Adding in rspec test for rescue statement in jobs

--- spec/jobs/job_builder_spec.rb
+++ spec/jobs/job_builder_spec.rb
@@ -6,7 +6,13 @@ RSpec.describe JobBuilder.new("Random") do
   it{is_expected.to be_truthy}
   it{expect(JobBuilder.new("Random").job).to be_truthy}
   it{expect(JobBuilder.new("Random").job).to be_kind_of(Object)}
-  # it{expect(JobBuilder.new("Random").job.respond_to?(:perform_later)).to be_truthy}
 
+  context "Will create a new job class" do
+    let(:job) { JobBuilder }
+    it "success" do
+      allow(Object).to receive(:const_get).and_raise(NameError)
+      expect(job.new("test")).to be_truthy
+    end
+  end
 
 end
