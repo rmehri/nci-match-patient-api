5189f8506062e66fd8644b8aa967181608c39a1e
josephverbeck <joseph.verbeck@nih.gov>
2017-05-02 12:13:42 -0400

    Fixing some unit tests

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -5,91 +5,95 @@ describe Ability, :type => :model do
   end
 
   describe "should handle roles correctly" do
-    it "not allow management when blank" do
-      expect(Ability.new().can?(:manage, :all)).to eq(false)
+    context "not allow management when blank" do
+      it{expect(Ability.new().can?(:manage, :all)).to eq(false)}
     end
 
-    it "allow admin to manage all" do
-      expect(Ability.new({:roles => ["ADMIN"]}).can?(:manage, :all)).to eq(true)
+    context "allow admin to manage all" do
+      it{expect(Ability.new({:roles => ["ADMIN"]}).can?(:manage, :all)).to eq(true)}
     end
 
-    it "allow PATIENT_MESSAGE_SENDER to access trigger method" do
-      expect(Ability.new({:roles => ["PATIENT_MESSAGE_SENDER"]}).can?(:trigger, NciMatchPatientModels)).to eq(true)
+    context "allow PATIENT_MESSAGE_SENDER to access trigger method" do
+      it{expect(Ability.new({:roles => ["PATIENT_MESSAGE_SENDER"]}).can?(:trigger, NciMatchPatientModels)).to eq(true)}
     end
 
-    it "allow PATIENT_MESSAGE_SENDER to access validate_json_message method" do
-      expect(Ability.new({:roles => ["PATIENT_MESSAGE_SENDER"]}).can?(:validate_json_message, TreatmentArmMessage)).to eq(true)
-      expect(Ability.new({:roles => ["PATIENT_MESSAGE_SENDER"]}).can?(:validate_json_message, CogMessage)).to eq(true)
+    context "allow PATIENT_MESSAGE_SENDER to access validate_json_message method" do
+      it{expect(Ability.new({:roles => ["PATIENT_MESSAGE_SENDER"]}).can?(:validate_json_message, TreatmentArmMessage)).to eq(true)}
+      it{expect(Ability.new({:roles => ["PATIENT_MESSAGE_SENDER"]}).can?(:validate_json_message, CogMessage)).to eq(true)}
     end
 
-    it "allow MochaVariantReportReviewer to access variant_report_status method" do
-      expect(Ability.new({:roles => ["MOCHA_VARIANT_REPORT_REVIEWER"]}).can?(:variant_report_status, :MoCha)).to eq(true)
+    context "allow MochaVariantReportReviewer to access variant_report_status method" do
+      it{expect(Ability.new({:roles => ["MOCHA_VARIANT_REPORT_REVIEWER"]}).can?(:variant_report_status, :MoCha)).to eq(true)}
     end
 
-    it "allow AssignmentReportReviewer to access assignment_confirmation method" do
-      expect(Ability.new({:roles => ["ASSIGNMENT_REPORT_REVIEWER"]}).can?(:assignment_confirmation, AssignmentStatusMessage)).to eq(true)
-      expect(Ability.new({:roles => ["ASSIGNMENT_REPORT_REVIEWER"]}).can?(:assignment_confirmation, NciMatchPatientModels)).to eq(true)
-      expect(Ability.new({:roles => ["ASSIGNMENT_REPORT_REVIEWER"]}).can?(:validate_json_message, AssignmentStatusMessage)).to eq(true)
-      expect(Ability.new({:roles => ["ASSIGNMENT_REPORT_REVIEWER"]}).can?(:validate_json_message, NciMatchPatientModels)).to eq(true)
+    context "allow AssignmentReportReviewer to access assignment_confirmation method" do
+      it{expect(Ability.new({:roles => ["ASSIGNMENT_REPORT_REVIEWER"]}).can?(:assignment_confirmation, AssignmentStatusMessage)).to eq(true)}
+      it{expect(Ability.new({:roles => ["ASSIGNMENT_REPORT_REVIEWER"]}).can?(:assignment_confirmation, NciMatchPatientModels)).to eq(true)}
+      it{expect(Ability.new({:roles => ["ASSIGNMENT_REPORT_REVIEWER"]}).can?(:validate_json_message, AssignmentStatusMessage)).to eq(true)}
+      it{expect(Ability.new({:roles => ["ASSIGNMENT_REPORT_REVIEWER"]}).can?(:validate_json_message, NciMatchPatientModels)).to eq(true)}
     end
 
 
-    it "allow System to access event and variant report" do
-      expect(Ability.new({:roles => ["SYSTEM"]}).can?(:validate_json_message, :Event)).to eq(true)
-      expect(Ability.new({:roles => ["SYSTEM"]}).can?(:validate_json_message, VariantReportMessage)).to eq(true)
-      expect(Ability.new({:roles => ["SYSTEM"]}).can?(:create, :Event)).to eq(true)
-      expect(Ability.new({:roles => ["SYSTEM"]}).can?(:create, VariantReportMessage)).to eq(true)
+    context "allow System to access event and variant report" do
+      it{expect(Ability.new({:roles => ["SYSTEM"]}).can?(:validate_json_message, :Event)).to eq(true)}
+      it{expect(Ability.new({:roles => ["SYSTEM"]}).can?(:validate_json_message, VariantReportMessage)).to eq(true)}
+      it{expect(Ability.new({:roles => ["SYSTEM"]}).can?(:create, :Event)).to eq(true)}
+      it{expect(Ability.new({:roles => ["SYSTEM"]}).can?(:create, VariantReportMessage)).to eq(true)}
+      it{expect(Ability.new({:roles => ["SYSTEM"]}).can?(:variant_report_uploaded, :MDA)).to eq(true)}
+      it{expect(Ability.new({:roles => ["SYSTEM"]}).can?(:variant_report_uploaded, :MoCha)).to eq(true)}
+      it{expect(Ability.new({:roles => ["SYSTEM"]}).can?(:variant_report_uploaded, :Dartmouth)).to eq(true)}
+      it{expect(Ability.new({:roles => ["SYSTEM"]}).can?(:variant_report_uploaded, VariantReportMessage)).to eq(true)}
     end
 
-    it "allow SpecimenMessageSender to access trigger method" do
-      expect(Ability.new({:roles => ["SPECIMEN_MESSAGE_SENDER"]}).can?(:trigger, NciMatchPatientModels)).to eq(true)
+    context "allow SpecimenMessageSender to access trigger method" do
+      it{expect(Ability.new({:roles => ["SPECIMEN_MESSAGE_SENDER"]}).can?(:trigger, NciMatchPatientModels)).to eq(true)}
     end
 
-    it "allow AssayMessageSender to access trigger method" do
-      expect(Ability.new({:roles => ["ASSAY_MESSAGE_SENDER"]}).can?(:trigger, NciMatchPatientModels)).to eq(true)
+    context "allow AssayMessageSender to access trigger method" do
+      it{expect(Ability.new({:roles => ["ASSAY_MESSAGE_SENDER"]}).can?(:trigger, NciMatchPatientModels)).to eq(true)}
     end
 
-    it "allow VariantReportSender to access variant_report_upload method" do
-      expect(Ability.new({:roles => ["MDA_VARIANT_REPORT_SENDER"]}).can?(:variant_report_uploaded, NciMatchPatientModels)).to eq(true)
+    context "allow VariantReportSender to access variant_report_upload method" do
+      it{expect(Ability.new({:roles => ["MDA_VARIANT_REPORT_SENDER"]}).can?(:variant_report_uploaded, NciMatchPatientModels)).to eq(true)}
     end
 
-    it "allow MdaVariantReportReviewer to access variant_report_status method" do
-      expect(Ability.new({:roles => ["MDA_VARIANT_REPORT_REVIEWER"]}).can?(:variant_report_status, VariantReportStatusMessage)).to eq(true)
-      expect(Ability.new({:roles => ["MDA_VARIANT_REPORT_REVIEWER"]}).can?(:variant_report_status, NciMatchPatientModels)).to eq(true)
-      expect(Ability.new({:roles => ["MDA_VARIANT_REPORT_REVIEWER"]}).can?(:validate_json_message, VariantReportStatusMessage)).to eq(true)
-      expect(Ability.new({:roles => ["MDA_VARIANT_REPORT_REVIEWER"]}).can?(:validate_json_message, NciMatchPatientModels)).to eq(true)
+    context "allow MdaVariantReportReviewer to access variant_report_status method" do
+      it{expect(Ability.new({:roles => ["MDA_VARIANT_REPORT_REVIEWER"]}).can?(:variant_report_status, VariantReportStatusMessage)).to eq(true)}
+      it{expect(Ability.new({:roles => ["MDA_VARIANT_REPORT_REVIEWER"]}).can?(:variant_report_status, NciMatchPatientModels)).to eq(true)}
+      it{expect(Ability.new({:roles => ["MDA_VARIANT_REPORT_REVIEWER"]}).can?(:validate_json_message, VariantReportStatusMessage)).to eq(true)}
+      it{expect(Ability.new({:roles => ["MDA_VARIANT_REPORT_REVIEWER"]}).can?(:validate_json_message, NciMatchPatientModels)).to eq(true)}
     end
 
   end
 
   describe "should lock down actions correctly" do
 
-    it "allow PATIENT_MESSAGE_SENDER to not access all method" do
-      expect(Ability.new({:roles => ["PATIENT_MESSAGE_SENDER"]}).can?(:manage, :all)).to eq(false)
+    context "allow PATIENT_MESSAGE_SENDER to not access all method" do
+      it{expect(Ability.new({:roles => ["PATIENT_MESSAGE_SENDER"]}).can?(:manage, :all)).to eq(false)}
     end
 
-    it "allow MochaVariantReportReviewer to access variant_report_status method" do
-      expect(Ability.new({:roles => ["MOCHA_VARIANT_REPORT_REVIEWER"]}).can?(:manage, :all)).to eq(false)
+    context "allow MochaVariantReportReviewer to access variant_report_status method" do
+      it{expect(Ability.new({:roles => ["MOCHA_VARIANT_REPORT_REVIEWER"]}).can?(:manage, :all)).to eq(false)}
     end
 
-    it "allow AssignmentReportReviewer to access assignment_confirmation method" do
-      expect(Ability.new({:roles => ["ASSIGNMENT_REPORT_REVIEWER"]}).can?(:manage, :all)).to eq(false)
+    context "allow AssignmentReportReviewer to access assignment_confirmation method" do
+      it{expect(Ability.new({:roles => ["ASSIGNMENT_REPORT_REVIEWER"]}).can?(:manage, :all)).to eq(false)}
     end
 
-    it "allow SpecimenMessageSender to access trigger method" do
-      expect(Ability.new({:roles => ["SPECIMEN_MESSAGE_SENDER"]}).can?(:manage, :all)).to eq(false)
+    context "allow SpecimenMessageSender to access trigger method" do
+      it{expect(Ability.new({:roles => ["SPECIMEN_MESSAGE_SENDER"]}).can?(:manage, :all)).to eq(false)}
     end
 
-    it "allow AssayMessageSender to access trigger method" do
-      expect(Ability.new({:roles => ["ASSAY_MESSAGE_SENDER"]}).can?(:manage, :all)).to eq(false)
+    context "allow AssayMessageSender to access trigger method" do
+      it{expect(Ability.new({:roles => ["ASSAY_MESSAGE_SENDER"]}).can?(:manage, :all)).to eq(false)}
     end
 
-    it "allow VariantReportSender to access trigger method" do
-      expect(Ability.new({:roles => ["VARIANT_REPORT_SENDER"]}).can?(:manage, :all)).to eq(false)
+    context "allow VariantReportSender to access trigger method" do
+      it{expect(Ability.new({:roles => ["VARIANT_REPORT_SENDER"]}).can?(:manage, :all)).to eq(false)}
     end
 
-    it "allow MdaVariantReportReviewer to access variant_report_status method" do
-      expect(Ability.new({:roles => ["MDA_VARIANT_REPORT_REVIEWER"]}).can?(:manage, :all)).to eq(false)
+    context "allow MdaVariantReportReviewer to access variant_report_status method" do
+      it{expect(Ability.new({:roles => ["MDA_VARIANT_REPORT_REVIEWER"]}).can?(:manage, :all)).to eq(false)}
     end
 
   end
