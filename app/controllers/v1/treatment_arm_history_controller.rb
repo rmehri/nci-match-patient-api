b3e1ac61d14ae53b02b5a816615a089666b978af
Shan Yang <shan.yang@nih.gov>
2017-05-15 20:54:27 -0400

    Fixed a bug in /treatment_arm_history

--- app/controllers/v1/treatment_arm_history_controller.rb
+++ app/controllers/v1/treatment_arm_history_controller.rb
@@ -6,17 +6,21 @@ module V1
       render json: instance_variable_get("@#{resource_name}")
     end
 
-
     private
-    def set_resource(resources = {})
-      resources = NciMatchPatientModels::Assignment.scan(resource_params).collect{|record| build_treatment_arms_history_model(record.to_h.compact) }
-      instance_variable_set("@#{resource_name}", resources)
+
+    def resource_name
+      @resource_name = "assignment"
     end
 
-    def treatment_arm_history_params
+    def set_resource(_resource = {})
+      resources = NciMatchPatientModels::Assignment.scan(resource_params).collect{|record| build_treatment_arms_history_model(record.to_h.compact) }
+      instance_variable_set("@#{resource_name}", resources.sort_by {|record| record[:assignment_date]}.reverse)
+    end
+
+    def assignment_params
       build_index_query({:patient_id => params.require(:patient_id),
-                   :projection => ['selected_treatment_arm', 'step_number', 'cog_assignment_date'],
-                   :attribute => ['selected_treatment_arm', 'step_number', 'cog_assignment_date']})
+                         :projection => ['selected_treatment_arm', 'step_number', 'cog_assignment_date'],
+                         :attribute => ['selected_treatment_arm', 'step_number', 'cog_assignment_date']})
     end
 
     def build_treatment_arms_history_model(record = {})
@@ -27,7 +31,8 @@ module V1
           :version => record[:selected_treatment_arm][:version],
           :step => record[:step_number],
           :assignment_reason => record[:selected_treatment_arm][:reason],
-          :assignment_date => record[:cog_assignment_date]
+          :date_on_arm => record[:cog_assignment_date],
+          :assignment_date => record[:assignment_date]
       }
     end
 
--- spec/controllers/v1/treatment_arm_history_controller_spec.rb
+++ spec/controllers/v1/treatment_arm_history_controller_spec.rb
@@ -4,22 +4,25 @@ describe V1::TreatmentArmHistoryController, :type => :controller do
   describe "should return a valid data set" do
 
     it "single set" do
-      allow(NciMatchPatientModels::Assignment).to receive(:scan).and_return([{:cog_assignment_date => Date.current ,:step_number => 1.1 ,
+      cog_assignment_date = Date.current.to_s
+      allow(NciMatchPatientModels::Assignment).to receive(:scan).and_return([{:cog_assignment_date => cog_assignment_date ,:step_number => 1.1 , :assignment_date => cog_assignment_date,
                                                                               :selected_treatment_arm => {:treatment_arm_id => "ABC", :version => "123", :stratum_id => "A", :reason => "Just because"}}])
       get :index, :patient_id => "123"
       expect(response).to have_http_status(200)
-      expect(JSON.parse(response.body)).to eq([{"treatment_arm_id"=>"ABC", "stratum_id"=>"A", "version"=>"123", "step"=>1.1, "assignment_reason"=>"Just because", "assignment_date"=>Date.current.to_s}])
+      expect(JSON.parse(response.body)).to eq([{"treatment_arm_id"=>"ABC", "stratum_id"=>"A", "version"=>"123", "step"=>1.1, "assignment_reason"=>"Just because", "date_on_arm"=>cog_assignment_date, "assignment_date"=>cog_assignment_date}])
     end
 
     it "multiple set" do
-      allow(NciMatchPatientModels::Assignment).to receive(:scan).and_return([{:cog_assignment_date => Date.current ,:step_number => 1.1 ,
-                                                                              :selected_treatment_arm => {:treatment_arm_id => "ABC", :version => "123", :stratum_id => "A", :reason => "Just because"}},
-                                                                             {:cog_assignment_date => Date.current ,:step_number => 2.1 ,
-                                                                              :selected_treatment_arm => {:treatment_arm_id => "CBA", :version => "321", :stratum_id => "C", :reason => "Just because"}}])
+      cog_assignment_date = Date.current
+      allow(NciMatchPatientModels::Assignment).to receive(:scan).and_return([{:cog_assignment_date => Date.current ,:step_number => 2.1 , :assignment_date => Date.current,
+                                                                               :selected_treatment_arm => {:treatment_arm_id => "CBA", :version => "321", :stratum_id => "C", :reason => "Just because"}},
+                                                                              {:cog_assignment_date => cog_assignment_date ,:step_number => 1.1 , :assignment_date => cog_assignment_date,
+                                                                                :selected_treatment_arm => {:treatment_arm_id => "ABC", :version => "123", :stratum_id => "A", :reason => "Just because"}}
+                                                                              ])
       get :index, :patient_id => "123"
       expect(response).to have_http_status(200)
       expect(JSON.parse(response.body).length).to eq(2)
-      expect(JSON.parse(response.body)).to eq([{"treatment_arm_id"=>"ABC", "stratum_id"=>"A", "version"=>"123", "step"=>1.1, "assignment_reason"=>"Just because", "assignment_date"=>Date.current.to_s}, {"treatment_arm_id"=>"CBA", "stratum_id"=>"C", "version"=>"321", "step"=>2.1, "assignment_reason"=>"Just because", "assignment_date"=>Date.current.to_s}])
+      expect(JSON.parse(response.body)).to eq([{"treatment_arm_id"=>"ABC", "stratum_id"=>"A", "version"=>"123", "step"=>1.1, "assignment_reason"=>"Just because", "date_on_arm"=>cog_assignment_date.to_s, "assignment_date"=>cog_assignment_date.to_s}, {"treatment_arm_id"=>"CBA", "stratum_id"=>"C", "version"=>"321", "step"=>2.1, "assignment_reason"=>"Just because", "date_on_arm"=>cog_assignment_date.to_s, "assignment_date"=>Date.current.to_s}])
     end
 
   end
