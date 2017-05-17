6c094ba00634f9367cd0ad99b42550877d6ec317
asampatoor <adithya.sampatoor@nih.gov>
2017-05-17 11:46:10 -0400

    Reverting back

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -2,7 +2,7 @@ Apipie.configure do |config|
   config.app_name                = 'NciPedMatchPatientApi'
   config.copyright               = '© 2017 PedMATCH'
   config.api_base_url            = 'api/v1'
-  config.doc_base_url            = '/apidoc'
+  config.doc_base_url            = '/apidocs'
   config.validate                = false
   config.use_cache               = Rails.env.test? || Rails.env.uat?
   config.api_controllers_matcher = File.join(Rails.root, 'app', 'controllers', 'v1', '*.rb')
