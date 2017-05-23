7cd8207029c568fcbf9328e18b41bafaa72ed754
asampatoor <adithya.sampatoor@nih.gov>
2017-05-23 13:49:31 -0400

    Including s3 bucket details in the healthz route

--- app/controllers/v1/health_checks_controller.rb
+++ app/controllers/v1/health_checks_controller.rb
@@ -6,19 +6,22 @@ module V1
       result['dynamodb_connection'] = dynamodb_connection
       result['queue_name'] = Rails.configuration.environment.fetch('queue_name')
       result['queue_connection'] = queue_connection
+      result['s3_bucket_name'] = Rails.configuration.environment.fetch('s3_bucket')
+      result['s3_url'] = Rails.configuration.environment.fetch('s3_url')
+      result['s3_connection'] = s3_connection
       render json: result
     end
 
     private
 
-    attr_reader :dynamodb_connection, :queue_connection
+    attr_reader :dynamodb_connection, :queue_connection, :s3_connection
 
     def dynamodb_connection
       begin
         dynamodb = Aws::DynamoDB::Client.new(retry_limit: 1)
-        'successfull' if dynamodb.list_tables.present?
+        'successful' if dynamodb.list_tables.present?
       rescue => _error
-        'unsuccessfull'
+        'unsuccessful'
       end
     end
 
@@ -30,11 +33,19 @@ module V1
         aws_secret_access_key = Rails.application.secrets.aws_secret_access_key
         creds = Aws::Credentials.new(access_key, aws_secret_access_key)
         client = Aws::SQS::Client.new(endpoint: end_point, region: region, credentials: creds)
-        'successfull' if client.get_queue_url(queue_name: Rails.configuration.environment.fetch('queue_name')).present?
+        'successful' if client.get_queue_url(queue_name: Rails.configuration.environment.fetch('queue_name')).present?
       rescue => _error
-        'unsuccessfull'
+        'unsuccessful'
+      end
+    end
+
+    def s3_connection
+      begin
+        bucket_name = Rails.configuration.environment.fetch('s3_bucket')
+        'successful' if Aws::S3::S3Reader.s3_client.bucket(bucket_name).present?
+      rescue => _error
+        'unsuccessful'
       end
     end
   end
 end
-
