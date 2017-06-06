6b7c83287c5550d7abba7b719700054a3887f154
Shan Yang <shan.yang@nih.gov>
2017-06-06 15:38:59 -0400

    Added a filter that removes patient that doesn't have specimen_received_date in active_specimen.

--- app/controllers/v1/patient_limbos_controller.rb
+++ app/controllers/v1/patient_limbos_controller.rb
@@ -28,6 +28,7 @@ module V1
                                                        :scan_filter => {"current_status" => {:comparison_operator => "IN", :attribute_value_list => target_statuses},
                                                                         "active_tissue_specimen" => {:comparison_operator => "NOT_NULL"}}}).collect { |data| data.to_h.compact.deep_symbolize_keys! }
 
+      resources = resources.select{ |resource| !(resource[:active_tissue_specimen][:specimen_received_date].nil?)}
       resources = resources.select{ |resource| (Date.current - Date.parse(resource[:active_tissue_specimen][:specimen_received_date])).to_i >= 7 }
       resources.uniq!{ |resource| resource[:patient_id] }
       instance_variable_set("@#{resource_name}", generate_messages(resources))
