d5d52fb873262f1eba6d9800853f41a907bf074a
josephverbeck <joseph.verbeck@nih.gov>
2017-06-15 14:24:07 -0400

    Adding in opt pass for json

--- app/controllers/v1/rollback_controller.rb
+++ app/controllers/v1/rollback_controller.rb
@@ -6,7 +6,7 @@ module V1
       is_valid = HTTParty.get("#{Rails.configuration.environment.fetch('patient_state_api')}/roll_back/variant_report/#{params[:patient_id]}",
                               {:headers => {'X-Request-Id' => request.uuid, 'Authorization' => "Bearer #{token}"}})
       raise Errors::RequestForbidden, "Incoming message failed patient state validation: #{is_valid}" if is_valid.code.to_i > 200
-      JobBuilder.new("RollBack::VariantReportJob").job.perform_later({:patient_id => params[:patient_id]})
+      JobBuilder.new("RollBack::VariantReportJob").job.perform_later(convert_request(request.raw_post, params[:patient_id]))
     end
 
 
@@ -15,7 +15,14 @@ module V1
       is_valid = HTTParty.get("#{Rails.configuration.environment.fetch('patient_state_api')}/roll_back/assignment/#{params[:patient_id]}",
                               {:headers => {'X-Request-Id' => request.uuid, 'Authorization' => "Bearer #{token}"}})
       raise Errors::RequestForbidden, "Incoming message failed patient state validation: #{is_valid}" if is_valid.code.to_i > 200
-      JobBuilder.new("RollBack::AssignmentReportJob").job.perform_later({:patient_id => params[:patient_id]})
+      JobBuilder.new("RollBack::AssignmentReportJob").job.perform_later(convert_request(request.raw_post, params[:patient_id]))
+    end
+
+    private
+    def convert_request(json, patient_id="")
+      message = JSON.parse(json).deep_transform_keys!(&:underscore).symbolize_keys
+      message[:patient_id] = patient_id
+      message
     end
 
   end
