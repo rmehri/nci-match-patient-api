c59b06c7f30db5cdae45abc0ec1a6700e8843926
Shan Yang <shan.yang@nih.gov>
2017-07-07 16:51:25 -0400

    Trying Damir's approach...

--- app/controllers/v1/pending_view_controller.rb
+++ app/controllers/v1/pending_view_controller.rb
@@ -3,73 +3,49 @@ module V1
     before_action :authenticate_user
 
     def pending_view
-      begin
+      variant_reports = NciMatchPatientModels::VariantReport.scan_and_find_by({status: 'PENDING'})
+      tissue_reports = []
 
-        variant_reports = NciMatchPatientModels::VariantReport.find_by({:status => 'PENDING'})
+      variant_reports.each do | variant_report |
 
-        tissue_reports = []
-        assignments = []
-        variant_reports.each do | variant_report |
-          next if variant_report.variant_report_type == 'BLOOD'
-
-          patient = NciMatchPatientModels::Patient.query_patient_by_id(variant_report.patient_id)
-          next if (patient.current_status.blank?)
-          next if (patient.current_status == "OFF_STUDY" || patient.current_status == "OFF_STUDY_BIOPSY_EXPIRED")
-
-          data = {:patient_id => variant_report.patient_id,
-                  :molecular_id => variant_report.molecular_id,
-                  :analysis_id => variant_report.analysis_id,
-                  :surgical_event_id => variant_report.surgical_event_id,
-                  :ion_reporter_id => variant_report.ion_reporter_id,
-                  :clia_lab => variant_report.clia_lab,
-                  :variant_report_received_date => variant_report.variant_report_received_date}
-          data[:specimen_received_date] = get_specimen_received_date(variant_report.patient_id,
-                                                                     variant_report.variant_report_type)
-          tissue_reports.push(data)
-        end
+        p "================ var report: #{variant_report}"
 
 
-        assignment_reports = NciMatchPatientModels::Assignment.find_by({:status => 'PENDING'})
+        next if variant_report['variant_report_type'] == 'BLOOD'
 
-        assignment_reports.each do | assignment_report|
+        p "=============================== p1"
+        patient = NciMatchPatientModels::Patient.query_patient_by_id(variant_report['patient_id'])
+        p "=============================== p2"
 
-          patient = NciMatchPatientModels::Patient.query_patient_by_id(assignment_report.patient_id)
-          next if (patient.current_status == "OFF_STUDY" || patient.current_status == "OFF_STUDY_BIOPSY_EXPIRED")
-
-          data = {:patient_id => assignment_report.patient_id,
-                  :molecular_id => assignment_report.molecular_id,
-                  :analysis_id => assignment_report.analysis_id,
-                  :surgical_event_id => assignment_report.surgical_event_id,
-                  :assignment_date => assignment_report.assignment_date,
-                  :assignment_uuid => assignment_report.uuid}
-
-          data[:disease] = get_patient_diseases(patient)
-          data = ApplicationHelper.merge_treatment_arm_fields(data, assignment_report.selected_treatment_arm)
-          assignments.push(data)
-        end
-
-        pending_view = {:tissue_variant_reports => tissue_reports,
-                        :assignment_reports => assignments}
-
-        render json: pending_view.to_json
-
-      rescue => error
-        standard_error_message(error)
+        next if (patient.current_status == "OFF_STUDY" || patient.current_status == "OFF_STUDY_BIOPSY_EXPIRED") || (patient.current_status.blank?)
+        p "=============================== p3"
+        tissue_report_data = variant_report.slice('patient_id', 'molecular_id', 'analysis_id', 'surgical_event_id', 'ion_reporter_id', 'clia_lab', 'variant_report_received_date')
+        p "=============================== p4"
+        tissue_reports.push tissue_report_data.merge(specimen_received_date: get_specimen_received_date(variant_report['patient_id'], variant_report['variant_report_type']))
       end
+
+      assignment_reports = NciMatchPatientModels::Assignment.scan_and_find_by({status: 'PENDING'})
+      assignments = []
+
+      assignment_reports.each do | assignment_report|
+
+        patient = NciMatchPatientModels::Patient.query_patient_by_id(assignment_report['patient_id'])
+        next if (patient.current_status == "OFF_STUDY" || patient.current_status == "OFF_STUDY_BIOPSY_EXPIRED")
+
+        assignment_data = assignment_report.slice('patient_id', 'molecular_id', 'analysis_id', 'surgical_event_id', 'assignment_date', 'uuid').merge(disease: ApplicationHelper.format_disease_names(patient.diseases))
+        assignments.push ApplicationHelper.merge_treatment_arm_fields(assignment_data, assignment_report['selected_treatment_arm'])
+      end
+
+      render json: {tissue_variant_reports: tissue_reports, assignment_reports: assignments}.to_json
     end
 
     def get_specimen_received_date(patient_id, type)
-      specimen = nil
-      if type == 'TISSUE'
-        specimen = NciMatchPatientModels::Specimen.query_latest_tissue_specimen_by_patient_id(patient_id)
-      else
-        specimen = NciMatchPatientModels::Specimen.query_latest_blood_specimen_by_patient_id(patient_id)
-      end
-      !specimen.nil? ? specimen.received_date : ""
-    end
 
-    def get_patient_diseases(patient)
-      ApplicationHelper.format_disease_names(patient.diseases)
+      p "======================== type: #{type} patient id: #{patient_id}"
+
+
+      specimen = (type == 'TISSUE' ? NciMatchPatientModels::Specimen.query_latest_tissue_specimen_by_patient_id(patient_id) : NciMatchPatientModels::Specimen.query_latest_blood_specimen_by_patient_id(patient_id))
+      specimen.nil? ? '' : specimen.received_date
     end
 
   end
--- spec/controllers/v1/pending_view_controller_spec.rb
+++ spec/controllers/v1/pending_view_controller_spec.rb
@@ -1,43 +1,69 @@
 describe V1::PendingViewController do
 
   it 'should return a pending view with 1 pending variant report' do
-    variant_report = NciMatchPatientModels::VariantReport.new
-    variant_report.patient_id = "3366"
-    variant_report.variant_report_received_date = DateTime.current.getutc().to_s
-    variant_report.analysis_id = "an-1234"
-    variant_report.molecular_id = "mo-1234"
-    variant_report.ion_reporter_id = "ion-1234"
-    variant_report.tsv_file_name = "4.tsv"
-    variant_report.status = 'PENDING'
-    variant_report.variant_report_type = 'TISSUE'
-    allow(NciMatchPatientModels::VariantReport).to receive(:scan).and_return([variant_report])
+    # variant_report = NciMatchPatientModels::VariantReport.new
+    # variant_report.patient_id = "3366"
+    # variant_report.variant_report_received_date = DateTime.current.getutc().to_s
+    # variant_report.analysis_id = "an-1234"
+    # variant_report.molecular_id = "mo-1234"
+    # variant_report.ion_reporter_id = "ion-1234"
+    # variant_report.tsv_file_name = "4.tsv"
+    # variant_report.status = 'PENDING'
+    # variant_report.variant_report_type = 'TISSUE'
+
+    vr = {"patient_id" => "3366",
+          "variant_report_received_date" => DateTime.current.getutc().to_s,
+          "analysis_id" => "an-1234",
+          "molecular_id" => "mo-1234",
+          "ion_reporter_id" => "ion-1234",
+          "tsv_file_name" => "4.tsv",
+          "status" => 'PENDING',
+          "variant_report_type" => 'TISSUE'}
+
+    allow(Aws::DynamoDB::Client).to receive(:new).and_return(Aws::DynamoDB::Client.new)
+
+    allow(NciMatchPatientModels::VariantReport).to receive(:scan_and_find_by).and_return([vr])
 
     patient = NciMatchPatientModels::Patient.new
     patient.patient_id = "3366"
     patient.current_status = "TISSUE_VARIANT_REPORT_RECEIVED"
     allow(NciMatchPatientModels::Patient).to receive(:query).and_return([patient])
 
-    allow(NciMatchPatientModels::Assignment).to receive(:scan).and_return([])
+    allow(NciMatchPatientModels::Assignment).to receive(:scan_and_find_by).and_return([])
     get :pending_view
     expect(response).to have_http_status(200)
 
-    pending_view = report = JSON.parse(response.body).deep_symbolize_keys
+    pending_view = JSON.parse(response.body).deep_symbolize_keys
     expect(pending_view[:tissue_variant_reports].length).to eq(1)
     expect(pending_view[:assignment_reports].length).to eq(0)
   end
 
   it 'should return a pending view with 1 pending assignment report' do
-    allow(NciMatchPatientModels::VariantReport).to receive(:scan).and_return([])
+    allow(Aws::DynamoDB::Client).to receive(:new).and_return(Aws::DynamoDB::Client.new)
 
-    assignment = NciMatchPatientModels::Assignment.new
-    assignment.patient_id = "3366"
-    assignment.assignment_date = DateTime.current.getutc().to_s
-    assignment.analysis_id = "an-1234"
-    assignment.molecular_id = "mo-1234"
-    assignment.status = 'PENDING'
-    assignment.surgical_event_id = 'event_id'
-    assignment.selected_treatment_arm = {:treatment_arm_id => 'A', :stratum_id => '1', :version => "2016"}
-    allow(NciMatchPatientModels::Assignment).to receive(:scan).and_return([assignment])
+    allow(NciMatchPatientModels::VariantReport).to receive(:scan_and_find_by).and_return([])
+
+    # assignment = NciMatchPatientModels::Assignment.new
+    # assignment.patient_id = "3366"
+    # assignment.assignment_date = DateTime.current.getutc().to_s
+    # assignment.analysis_id = "an-1234"
+    # assignment.molecular_id = "mo-1234"
+    # assignment.status = 'PENDING'
+    # assignment.surgical_event_id = 'event_id'
+    # assignment.selected_treatment_arm = {:treatment_arm_id => 'A', :stratum_id => '1', :version => "2016"}
+
+    assignment = {
+        "patient_id" => "3366",
+        "assignment_date" => DateTime.current.getutc().to_s,
+        "analysis_id" => "an-1234",
+        "molecular_id" => "mo-1234",
+        "status" => 'PENDING',
+        "surgical_event_id" => 'event_id',
+        "selected_treatment_arm" => {"treatment_arm_id" => 'A', "stratum_id" => '1', "version" => "2016"}
+    }
+
+
+    allow(NciMatchPatientModels::Assignment).to receive(:scan_and_find_by).and_return([assignment])
 
     patient = NciMatchPatientModels::Patient.new
     patient.patient_id = "3366"
