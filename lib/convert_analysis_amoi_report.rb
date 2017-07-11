a650920d1a921881189e0dc060f72bcf3ded1e26
josephverbeck <joseph.verbeck@nih.gov>
2017-07-11 16:00:11 -0400

    Should be a working model now

--- lib/convert_analysis_amoi_report.rb
+++ lib/convert_analysis_amoi_report.rb
@@ -1,24 +1,26 @@
 module Convert
   class AmoisRuleModel
-    def self.to_ui_model(analysis_id, amois_from_rule=nil)
-      return {} if analysis_id.blank?
-      if amois_from_rule.blank?
-        map = {:total_amois => 0, :snv_indels => [], :copy_number_variants => [], :gene_fusions => []}
-        variants = NciMatchPatientModels::Variant.find_by({:analysis_id => analysis_id})
-        variants.each do | variant |
-          map_variant_type(variant).each do |k,v| map[k].push(v) end
-        end
-        return map
-      else
-        amois_from_rule.deep_symbolize_keys!
-        snv_indel_amois = amois_only(amois_from_rule[:snv_indels])
-        cnv_amois = amois_only(amois_from_rule[:copy_number_variants])
-        fusion_amois = amois_only(amois_from_rule[:gene_fusions])
+    def self.to_ui_model(amois_from_rule=nil)
+      return {} if amois_from_rule.blank?
+      amois_from_rule.deep_symbolize_keys!
+      snv_indel_amois = amois_only(amois_from_rule[:snv_indels])
+      cnv_amois = amois_only(amois_from_rule[:copy_number_variants])
+      fusion_amois = amois_only(amois_from_rule[:gene_fusions])
 
-        count = snv_indel_amois.length + cnv_amois.length + fusion_amois.length
+      count = snv_indel_amois.length + cnv_amois.length + fusion_amois.length
 
-        return {:total_amois => count, :snv_indels => snv_indel_amois, :copy_number_variants => cnv_amois, :gene_fusions => fusion_amois}
+      return {:total_amois => count, :snv_indels => snv_indel_amois, :copy_number_variants => cnv_amois, :gene_fusions => fusion_amois}
+    end
+
+    def self.find_amois(variants=nil)
+      return {} if mois.blank?
+      variants.deep_symbolize_keys!
+      formated_amois = {:snv_indels => [], :copy_number_variants => [], :gene_fusions => []}
+      variants.each do | variant |
+        map_variant_type(variant).each do |k,v| formated_amois[k].push(v) end
       end
+      formated_amois[:total_amois] = formated_amois.map{|k,v| map[k].length},reduce(:+)
+      return formated_amois
     end
 
     def self.amois_only(variants_by_type)
