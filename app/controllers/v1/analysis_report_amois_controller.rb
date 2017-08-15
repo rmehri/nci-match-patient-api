fd57381157beb0ce8535a4b64772907a9e515f10
Damir Roso <damir.roso@nih.gov>
2017-08-15 12:55:04 -0400

    AmoisAnalysisReport refactor/cleanup

--- app/controllers/v1/analysis_report_amois_controller.rb
+++ app/controllers/v1/analysis_report_amois_controller.rb
@@ -10,17 +10,18 @@ module V1
       variant_report = NciMatchPatientModels::VariantReport.query_by_analysis_id(params[:patient_id], params[:id])
       raise Errors::ResourceNotFound unless variant_report
 
-      # get mois, update variant_report and cache result
+      # process mois from rule, update variant_report and cache result
       cached_amois_stats = MemoryCache.memoize(variant_report.to_h) do
 
-        # get amois from rule engine
+        # get mois from rule engine
         mois_from_rule = VariantReportUpdater.updated_variant_report(variant_report.to_h.deep_symbolize_keys!, request.uuid, token)
 
-        # update amois count in variant_report
-        AmoisAnalysisReport.new(variant_report, mois_from_rule).update_amoi_count_in_variant_report!
+        # update_mois will set amois counts and uuids; it will also update its count in variant_report
+        report = AmoisAnalysisReport.new(variant_report, mois_from_rule)
+        report.update_mois!
 
         # cache result for print: {:total_amois => n, :snv_indels => [...], :copy_number_variants => [...], :gene_fusions => [...]}
-        Convert::AmoisRuleModel.to_ui(mois_from_rule)
+        Convert::AmoisRuleModel.to_ui(report.updated_mois)
       end
 
       # render output
--- app/models/amois_analysis_report.rb
+++ app/models/amois_analysis_report.rb
@@ -1,52 +1,53 @@
 class AmoisAnalysisReport
-  attr_accessor :variant_report, :variant_report_hash, :mois, :amoi_count
+  attr_accessor :variant_report, :variant_report_hash, :updated_mois, :amoi_count
 
   # init state
-  def initialize(variant_report, mois)
+  def initialize(variant_report, mois_from_rule)
     self.variant_report = variant_report
     self.variant_report_hash = variant_report.to_h.deep_symbolize_keys!
-    self.mois = mois
+    self.updated_mois = mois_from_rule.deep_dup # do not mutate input
     self.amoi_count = 0
   end
 
-  # update count in variant_report if its changed
-  def update_amoi_count_in_variant_report!
-    self.count_amois
+  # set amois count and uuids; update variant_report
+  def update_mois!
+    self.update_mois_groups
     return if variant_report.total_amois == amoi_count # no count change
     variant_report.total_amois = amoi_count
     variant_report.save
     AppLogger.log(self.class.name, "Amoi count updated for patient [#{variant_report.patient_id}]")
   end
 
-  # total count is count in each amoi group
-  def count_amois
-    self.amoi_count  = find_amoi_count_by_uuid(mois[:snv_indels])
-    self.amoi_count += find_amoi_count_by_uuid(mois[:copy_number_variants])
-    self.amoi_count += find_amoi_count_by_uuid(mois[:gene_fusions])
+  # process each mois group
+  def update_mois_groups
+    count_amois_and_set_uuids(updated_mois[:snv_indels])
+    count_amois_and_set_uuids(updated_mois[:copy_number_variants])
+    count_amois_and_set_uuids(updated_mois[:gene_fusions])
   end
 
-  # group count by matching mois with uuid
-  def find_amoi_count_by_uuid(mois_group)
-    amoi_count = 0
-    return amoi_count if mois_group.blank?
+  # count and set uuid
+  def count_amois_and_set_uuids(mois_group)
+    return if mois_group.blank?
 
     mois_group.each do | moi |
       next if moi[:amois].blank?
 
-      amoi_count += 1
+      # increment count if amoi is found
+      self.amoi_count += 1
+
+      # build query params to get variant for uuid
       query_hash = {  patient_id:   variant_report_hash[:patient_id],
                       molecular_id: variant_report_hash[:molecular_id],
                       analysis_id:  variant_report_hash[:analysis_id],
                       variant_type: moi[:variant_type]}
-
       query_hash[:surgical_event_id] = variant_report_hash[:surgical_event_id] if variant_report_hash[:surgical_event_id].present?
-      %i(identifier func_gene chromosome position reference alternative driver_gene partner_gene).each{|moi_attr| query_hash[moi_attr] = moi[moi_attr] if moi[moi_attr].present?}
+      %i(identifier func_gene chromosome position reference alternative driver_gene partner_gene).each do |moi_attr|
+        query_hash[moi_attr] = moi[moi_attr] if moi[moi_attr].present?
+      end
 
-      # this does not have any effect !
+      # set uuid
       variants = NciMatchPatientModels::Variant.find_by(query_hash).collect { |data| data.to_h.compact }
       moi[:uuid] = variants[0][:uuid] if (variants.length > 0)
     end
-
-    amoi_count
   end
 end
