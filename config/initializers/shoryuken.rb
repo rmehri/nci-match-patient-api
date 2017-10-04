a881723c1712a999a3e3ac5a9b49837a3d11bfdb
Shan Yang <shan.yang@nih.gov>
2017-10-04 13:52:45 -0400

    Restore Shoryuken config

--- config/initializers/shoryuken.rb
+++ config/initializers/shoryuken.rb
@@ -1,11 +1,11 @@
-# Shoryuken.configure_server do | config |
-#   logger = Shoryuken::Logging.initialize_logger(STDOUT)
-#   logger.formatter = Proc.new { |severity, datetime, progname, msg| "[#{SecureRandom.uuid}] [#{datetime.strftime("%B %d %H:%M:%S")}] [#{$$}] [#{severity}] [#{Rails.application.class.parent_name}], #{msg}\n"}
-#   logger.level = Logger::INFO
-#   Rails.logger = logger
-# end
+Shoryuken.configure_server do | config |
+  logger = Shoryuken::Logging.initialize_logger(STDOUT)
+  logger.formatter = Proc.new { |severity, datetime, progname, msg| "[#{SecureRandom.uuid}] [#{datetime.strftime("%B %d %H:%M:%S")}] [#{$$}] [#{severity}] [#{Rails.application.class.parent_name}], #{msg}\n"}
+  logger.level = Logger::INFO
+  Rails.logger = logger
+end
 
-# region = Rails.configuration.environment.fetch('aws_region')
-# end_point = "https://sqs.#{region}.amazonaws.com"
-#
-# Shoryuken.sqs_client = Aws::SQS::Client.new(endpoint: end_point, region: region)
+region = Rails.configuration.environment.fetch('aws_region')
+end_point = "https://sqs.#{region}.amazonaws.com"
+
+Shoryuken.sqs_client = Aws::SQS::Client.new(endpoint: end_point, region: region)
